c9aeb399b3b4dd6c24a86f4294e6b4effb395d6a
dteviot <dteviot@gmail.com>
2016-05-23 19:29:19 +1200

    Handle case when Baka-Tsuki hostname lacks www. prefix.

--- plugin/js/ParserFactory.js
+++ plugin/js/ParserFactory.js
@@ -8,16 +8,20 @@ var parserFactory = (function () {
 
     let parsers = new Map();
 
-    var register = function(hostName, constructor) {
-        if (parsers.get(hostName) == null) {
-            parsers.set(hostName, constructor);
+    var stripLeadingWww = function(hostName) {
+        return hostName.startsWith("www.") ? hostName.substring(4) : hostName;            
+    }
+
+    var register = function (hostName, constructor) {
+        if (parsers.get(stripLeadingWww(hostName)) == null) {
+            parsers.set(stripLeadingWww(hostName), constructor);
         } else {
             throw new Error("Duplicate parser registered for url " + url);
         };
     };
 
     var fetch = function(url) {
-        let hostName = extractHostName(url);
+        let hostName = stripLeadingWww(extractHostName(url));
         let constructor = parsers.get(hostName);
         return (constructor === undefined) ? undefined : constructor();
     };
