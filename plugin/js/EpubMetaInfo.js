21b1b6842be893f88a061be18afdf3d2466e3147
dteviot <dteviot@gmail.com>
2016-07-17 10:21:07 +1200

    Updated default style sheet.

--- plugin/js/EpubMetaInfo.js
+++ plugin/js/EpubMetaInfo.js
@@ -30,10 +30,78 @@ class EpubMetaInfo {
     }
 
     static getDefaultStyleSheet() {
-        return "img { max-width: 100%; max-height: 100%; padding: 0; margin: 0; } " +
-               "div.svg_outer { display: block; margin-bottom: 0; margin-left: 0; margin-right: 0; margin-top: 0; padding-bottom: 0; padding-left: 0; "+
-                           "padding-right: 0; padding-top: 0; text-align: left } " +
-               "div.svg_inner { display: block; text-align: center } "
-        ;
+        return ""+
+        // Style for svg images. I got this from BTE-Gen epunbs. Works nicely.
+        "div.svg_outer {\r"+
+        "   display: block;\r"+
+        "   margin-bottom: 0;\r"+
+        "   margin-left: 0;\r"+
+        "   margin-right: 0;\r"+
+        "   margin-top: 0;\r"+
+        "   padding-bottom: 0;\r"+
+        "   padding-left: 0;\r"+
+        "   padding-right: 0;\r"+
+        "   padding-top: 0;\r"+
+        "   text-align: left;\r"+
+        "}\r"+
+        "div.svg_inner {\r"+
+        "   display: block;\r"+
+        "   text-align: center;\r"+
+        "}\r"+
+
+        // Centered headings and some margin to make sure it's not too close to the content.
+        "h1, h2 {\r"+
+        "   text-align: center;\r"+
+        "   page-break-before: always;\r"+
+        "   margin-bottom: 10%;\r"+
+        "   margin-top: 10%;\r"+
+        "}\r"+
+        "h3, h4, h5, h6 {\r"+
+        "   text-align: center;\r"+
+        "   margin-bottom: 15%;\r"+
+        "   margin-top: 10%;\r"+
+        "}\r"+
+
+        // Style for lists. Calibre sometimes has issues with the placement of lists, this fixes it.
+        "ol, ul {\r"+
+        "   padding-left: 8%;\r"+
+        "}\r"+
+
+        // In case there are images in img tags. It's up to you
+        "img {\r"+
+        "   display: block;\r"+
+        "   max-height: 100%;\r"+
+        "   max-width: 100%;\r"+
+        "   padding-bottom: 0;\r"+
+        "   padding-left: 0;\r"+
+        "   padding-right: 0;\r"+
+        "   padding-top: 0;\r"+
+        "   margin-left: auto;\r"+
+        "   margin-right: auto;\r"+
+        "   margin-bottom: 2%;\r"+
+        "   margin-top: 2%;\r"+
+        "}\r"+
+
+        // To make hr tags more visible. BT doesn't use them very often but other sites might.
+        "hr {\r"+
+        "   color: black;\r"+
+        "   background-color: black;\r"+
+        "   height: 2px;\r"+
+        "}\r"+
+
+        // Styling all links.
+        "a:link {\r"+
+        "   text-decoration: none;\r"+
+        "   color: #0B0080;\r"+
+        "}\r"+
+        "a:visited {\r"+
+        "   text-decoration: none;\r"+
+        "}\r"+
+        "a:hover {\r"+
+        "   text-decoration: underline;\r"+
+        "}\r"+
+        "a:active {\r"+
+        "   text-decoration: underline;\r"+
+        "}";
     }
 }
