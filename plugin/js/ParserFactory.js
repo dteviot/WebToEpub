beecf5c518cdafce2c9e0ee1c595d0f8cecf72fd
dteviot <dteviot@gmail.com>
2016-09-08 21:14:06 +1200

    Changed ParserFactory to class syntax.
    And added registerRule()

--- plugin/js/ParserFactory.js
+++ plugin/js/ParserFactory.js
@@ -4,42 +4,55 @@
 
 "use strict";
 
-var parserFactory = (function () {
-
-    let parsers = new Map();
-
-    var isWebArchive = function(hostName) {
-        return util.extractHostName(hostName).startsWith("web.archive.org");
+class ParserFactory{
+    constructor() {
+        this.parsers = new Map();
+        this.parserRules = [];
     }
 
-    var stripWebArchive = function(url) {
+    static isWebArchive(url) {
+        return util.extractHostName(url).startsWith("web.archive.org");
+    }
+
+    static stripWebArchive(url) {
         var hostName = url.split('://');
         return hostName[2] ? "https://" + hostName[2] : url; 
     }
 
-    var stripLeadingWww = function(hostName) {
+    static stripLeadingWww(hostName) {
         return hostName.startsWith("www.") ? hostName.substring(4) : hostName;            
     }
 
-    var register = function (hostName, constructor) {
-        if (parsers.get(stripLeadingWww(hostName)) == null) {
-            parsers.set(stripLeadingWww(hostName), constructor);
+    register(hostName, constructor) {
+        if (this.parsers.get(ParserFactory.stripLeadingWww(hostName)) == null) {
+            this.parsers.set(ParserFactory.stripLeadingWww(hostName), constructor);
         } else {
             throw new Error("Duplicate parser registered for hostName " + hostName);
         };
-    };
+    }
 
-    var fetch = function(url) {
-        if (isWebArchive(url)) {
-            url = stripWebArchive(url);
+    /*
+    *  @param {function} test predicate that checks if parser can handle URL
+    *  @param {function} constructor to obtain parser to handle the URL
+    */
+    registerRule(test, constructor) {
+        this.parserRules.push( {test: test, constructor: constructor } );
+    }
+
+    fetch(url) {
+        if (ParserFactory.isWebArchive(url)) {
+            url = ParserFactory.stripWebArchive(url);
         }
-        let hostName = stripLeadingWww(util.extractHostName(url));
-        let constructor = parsers.get(hostName);
+        let hostName = ParserFactory.stripLeadingWww(util.extractHostName(url));
+        let constructor = this.parsers.get(hostName);
+        // no exact match found, see if any parser is willing to handle the URL
+        if (constructor === undefined) {
+            for (let pair of this.parserRules.filter(p => p.test(url))) {
+                return pair.constructor();
+            }
+        }
         return (constructor === undefined) ? undefined : constructor();
-    };
+    }
+}
 
-    return {
-        register: register,
-        fetch: fetch
-    };
-})();
+let parserFactory = new ParserFactory();
