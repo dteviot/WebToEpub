c4ac325ac1d896518942600702f7ca7fca19790f
dteviot <dteviot@gmail.com>
2016-12-13 20:04:29 +1300

    LightNovelBastionParser: Add title for each chapter.
    See: https://github.com/dteviot/WebToEpub/issues/91

--- plugin/js/parsers/LightNovelBastionParser.js
+++ plugin/js/parsers/LightNovelBastionParser.js
@@ -23,6 +23,24 @@ class LightNovelBastionParser extends Parser {
         return util.getElement(dom, "section");
     }
 
+    findChapterTitle(dom) {
+        let titleDiv = dom.createElement("div");
+        let titleText = util.getElement(dom, "title").textContent;
+        if (titleText !== "") {
+            let title = dom.createElement("h1");
+            title.appendChild(dom.createTextNode(titleText));
+            titleDiv.appendChild(title);
+        };
+        let header = util.getElement(dom, "header", e => e.id === "releases");
+        if (header !== null) {
+            let subtitle = util.getElement(header, "h2");
+            if (subtitle !== null) {
+                titleDiv.appendChild(subtitle);
+            }
+        }
+        return titleDiv;
+    }
+
     populateUI(dom) {
         super.populateUI(dom);
         CoverImageUI.showCoverImageUrlInput(true);
