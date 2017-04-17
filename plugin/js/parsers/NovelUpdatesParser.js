1c3b821a0f853371e0325be9b10b63156247ab0e
dteviot <dteviot@gmail.com>
2017-04-17 15:03:34 +1200

    Prototype parser for NovelUpdates.

---
+++ plugin/js/parsers/NovelUpdatesParser.js
@@ -0,0 +1,105 @@
+/*
+  Template to use to create a new parser
+*/
+"use strict";
+
+parserFactory.register("novelupdates.com", function() { return new NovelUpdatesParser() });
+
+class NovelUpdatesParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    // returns promise with the URLs of the chapters to fetch
+    // promise is used because may need to fetch the list of URLs from internet
+    getChapterUrls(dom) {
+        return NovelUpdatesParser.fetchChapterUrls(dom).then(function (links) {
+            let chapters = links.map(l => util.hyperLinkToChapter(l, null));
+            return Promise.resolve(chapters.reverse());
+        })
+    };
+
+    static fetchChapterUrls(dom) {
+        let extraPagesWithToc = NovelUpdatesParser.findPagesWithToC(dom);
+        return Promise.all(
+            extraPagesWithToc.map(url => NovelUpdatesParser.fetchChapterListFromPage(url))
+        ).then(function (chapterLists) {
+            return chapterLists.reduce(function (prev, current) {
+                return prev.concat(current);
+            }, NovelUpdatesParser.chapterLinksFromDom(dom))
+        });
+    }
+
+    static fetchChapterListFromPage(url) {
+        return HttpClient.wrapFetch(url).then(function (xhr) {
+            return Promise.resolve(NovelUpdatesParser.chapterLinksFromDom(xhr.responseXML))
+        });
+    }
+
+    static chapterLinksFromDom(dom) {
+        let table = util.getElement(dom, "table", e => e.id === "myTable");
+        let body = util.getElement(table, "tbody");
+        let links = [];
+        for(let row of util.getElements(body, "tr")) {
+            let link = NovelUpdatesParser.chapterLinksFromRow(row);
+            if (link !== null) {
+                links.push(link);
+            }
+        }
+        return links;
+    }
+
+    static chapterLinksFromRow(row) {
+        let links = util.getElements(row, "a");
+        return (0 < links.length) ? links[links.length - 1] : null;
+    }
+    
+    static findPagesWithToC(dom) {
+        let urls = [];
+        let div = util.getElement(dom, "div", e => e.className === "digg_pagination");
+        if (div !== null) {
+            let maxPage = NovelUpdatesParser.getPageValueOfLastTocPage(div);
+            for(let i = 2; i <= maxPage; ++i) {
+                urls.push(dom.baseURI + "?pg=" + i);
+            }
+        }
+        return urls;
+    }
+
+    static getPageValueOfLastTocPage(div) {
+        return util.getElements(div, "a").reduce(function(prev, curr) {
+            let t = NovelUpdatesParser.getPageSearchParameter(curr);
+            return (t > prev) ? t : prev;
+        }, -1);
+    }
+
+    static getPageSearchParameter(link) {
+        let query = link.search;
+        if (query.startsWith("?pg=")) {
+            return parseInt(query.substring(4));
+        }
+    } 
+
+    // returns the element holding the story content in a chapter
+    findContent(dom) {
+        return dom.body;
+    };
+
+    // title of the story
+    extractTitle(dom) {
+        let title = util.getElement(dom, "div", e => e.className === "seriestitlenu");
+        return (title === null) ? super.extractTitle(dom) : title.textContent;
+    };
+
+    // author of the story
+    extractAuthor(dom) {
+        let authors = util.getElement(dom, "div", e => e.id === "showauthors");
+        if (authors !== null) {
+            let author = util.getElement(authors, "a");
+            if (author !== null) {
+                return author.textContent;
+            }
+        }
+        return super.extractAuthor(dom);
+    };
+}
