dteviot/WebToEpub

commit fc17148ec8e58798684cf1ad6968f91a983c0dd7

Renaming chapter to webPage (where appropriate)

plugin/js/ChapterUrlsUI.js
@@ -163,7 +163,7 @@ class ChapterUrlsUI {
     setTableMode() {
         try {
             let chapters = this.htmlToChapters(ChapterUrlsUI.getEditChaptersUrlsInput().value);
-            this.parser.chapters = chapters;
+            this.parser.setPagesToFetch(chapters);
             this.populateChapterUrlsTable(chapters);
             this.usingTable = true;
             this.setVisibileUI(this.usingTable);
@@ -175,7 +175,7 @@ class ChapterUrlsUI {
     /** @private */
     reverseUrls() {
         try {
-            let chapters = this.parser.chapters;
+            let chapters = this.parser.getPagesToFetch().values();
             chapters.reverse();
             this.populateChapterUrlsTable(chapters);
         } catch (err) {
@@ -199,8 +199,8 @@ class ChapterUrlsUI {
         this.usingTable = false;
         this.setVisibileUI(this.usingTable);
         let input = ChapterUrlsUI.getEditChaptersUrlsInput();
-        input.rows = Math.max(this.parser.chapters.length, 20);
-        input.value = this.chaptersToHTML(this.parser.chapters);
+        input.rows = Math.max(this.parser.getPagesToFetch().size, 20);
+        input.value = this.chaptersToHTML(this.parser.getPagesToFetch().values());
     }
 
     chaptersToHTML(chapters) {

plugin/js/Parser.js
@@ -3,19 +3,39 @@
 */
 "use strict";
 
-class Parser {
-    constructor(imageCollector) {
-        this.chapters = [];
-        this.imageCollector = imageCollector || new ImageCollector();
-        this.userPreferences = null;
+/**
+ * A Parser's state variables
+*/
+class ParserState {
+    constructor() {
+        this.webPages = new Map();
         this.chapterListUrl = null;
     }
 
+    setPagesToFetch(urls) {
+        this.webPages = new Map(urls.map(u => [u.sourceUrl, u]));
+    }
+}
+
+class Parser {
+    constructor(imageCollector) {
+        this.state = new ParserState();
+        this.imageCollector = imageCollector || new ImageCollector();
+        this.userPreferences = null;
+    }
+
     copyState(otherParser) {
-        this.chapters = otherParser.chapters;
+        this.state = otherParser.state;
         this.imageCollector.copyState(otherParser.imageCollector);
         this.userPreferences = otherParser.userPreferences;
-        this.chapterListUrl = otherParser.chapterListUrl;
+    }
+
+    setPagesToFetch(urls) {
+        this.state.setPagesToFetch(urls);
+    }
+
+    getPagesToFetch() {
+        return this.state.webPages;
     }
 
     onUserPreferencesUpdate(userPreferences) {
@@ -23,34 +43,34 @@ class Parser {
         this.imageCollector.onUserPreferencesUpdate(userPreferences);
     }
 
-    isChapterPackable(chapter) {
-        return (chapter.rawDom != null) && (chapter.isIncludeable);
+    isWebPagePackable(webPage) {
+        return (webPage.rawDom != null) && (webPage.isIncludeable);
     }
 
-    convertRawDomToContent(chapter) {
+    convertRawDomToContent(webPage) {
         let that = this;
-        let content = that.findContent(chapter.rawDom);
-        that.customRawDomToContentStep(chapter, content);
+        let content = that.findContent(webPage.rawDom);
+        that.customRawDomToContentStep(webPage, content);
         that.removeUnwantedElementsFromContentElement(content);
-        this.addTitleToContent(chapter, content);
+        this.addTitleToContent(webPage, content);
         util.fixBlockTagsNestedInInlineTags(content);
         that.imageCollector.replaceImageTags(content);
         util.removeUnusedHeadingLevels(content);
         util.removeUnneededIds(content);
-        util.makeHyperlinksRelative(chapter.rawDom.baseURI, content);
+        util.makeHyperlinksRelative(webPage.rawDom.baseURI, content);
         util.setStyleToDefault(content);
         util.prepForConvertToXhtml(content);
         util.removeEmptyDivElements(content);
         util.removeTrailingWhiteSpace(content);
         if (util.isElementWhiteSpace(content)) {
-            let errorMsg = chrome.i18n.getMessage("warningNoVisibleContent", [chapter.sourceUrl]);
+            let errorMsg = chrome.i18n.getMessage("warningNoVisibleContent", [webPage.sourceUrl]);
             ErrorLog.showErrorMessage(errorMsg);
         }
         return content;
     }
 
-    addTitleToContent(chapter, content) {
-        let title = this.findChapterTitle(chapter.rawDom);
+    addTitleToContent(webPage, content) {
+        let title = this.findChapterTitle(webPage.rawDom);
         if (title !== null) {
             content.insertBefore(title, content.firstChild);
         };
@@ -81,7 +101,7 @@ class Parser {
 
     populateUI(dom) {
         this.getFetchContentButton().onclick = this.onFetchChaptersClicked.bind(this);
-        document.getElementById("packRawButton").onclick = this.packRawChapters.bind(this);
+        document.getElementById("packRawButton").onclick = this.packRawWebPages.bind(this);
         let coverUrl = this.findCoverImageUrl(dom);
         if (!util.isNullOrEmpty(coverUrl)) {
             CoverImageUI.setCoverImageUrl(coverUrl);
@@ -106,27 +126,22 @@ class Parser {
 
     removeNextAndPreviousChapterHyperlinks(element) {
         if (this.findParentNodeOfChapterLinkToRemoveAt != null) {
-            let that = this;
-            let chapterLinks = new Set();
-            for(let c of that.chapters) { 
-                chapterLinks.add(util.normalizeUrl(c.sourceUrl));
-            };
-
-            for(let unwanted of util.getElements(element, "a", link => chapterLinks.has(util.normalizeUrl(link.href)))
-                .map(link => that.findParentNodeOfChapterLinkToRemoveAt(link))) {
+            for(let unwanted of [...element.querySelectorAll("a")]
+                .filter(link => this.state.webPages.has(util.normalizeUrl(link.href)))
+                .map(link => this.findParentNodeOfChapterLinkToRemoveAt(link))) {
                 unwanted.remove();
             };
         }
     }
 
     /**
-    * default implementation turns each chapter into single epub item
+    * default implementation turns each webPage into single epub item
     */
-    chapterToEpubItems(chapter, epubItemIndex) {
-        let content = this.convertRawDomToContent(chapter);
+    webPageToEpubItems(webPage, epubItemIndex) {
+        let content = this.convertRawDomToContent(webPage);
         let items = [];
         if (content != null) {
-            items.push(new ChapterEpubItem(chapter, content, epubItemIndex));
+            items.push(new ChapterEpubItem(webPage, content, epubItemIndex));
         }
         return items;
     }
@@ -188,23 +203,22 @@ class Parser {
     }
 
     epubItemSupplier() {
-        let that = this;
-        let epubItems = that.chaptersToEpubItems(that.chapters);
-        let supplier = new EpubItemSupplier(this, epubItems, that.imageCollector);
+        let epubItems = this.webPagesToEpubItems(this.state.webPages);
+        let supplier = new EpubItemSupplier(this, epubItems, this.imageCollector);
         return supplier;
     }
 
-    chaptersToEpubItems(chapters) {
+    webPagesToEpubItems(webPages) {
         let epubItems = [];
         let index = 0;
         let initialHostName = this.initialHostName();
 
-        for(let chapter of chapters.filter(c => this.isChapterPackable(c))) {
-            let pageParser = this.parserForChapter(initialHostName, chapter);
-            let newItems = pageParser.chapterToEpubItems(chapter, index);
+        for(let webPage of webPages.filter(c => this.isWebPagePackable(c))) {
+            let pageParser = this.parserForWebPage(initialHostName, webPage);
+            let newItems = pageParser.webPageToEpubItems(webPage, index);
             epubItems = epubItems.concat(newItems);
             index += newItems.length;
-            delete(chapter.rawDom);
+            delete(webPage.rawDom);
         }
         return epubItems;
     }
@@ -212,14 +226,14 @@ class Parser {
     // called when plugin has obtained the first web page
     onLoadFirstPage(url, firstPageDom) {
         let that = this;
-        this.chapterListUrl = url;
+        this.state.chapterListUrl = url;
         
         // returns promise, because may need to fetch additional pages to find list of chapters
         that.getChapterUrls(firstPageDom).then(function(chapters) {
             if (that.userPreferences.chaptersPageInChapterList.value) {
-                chapters = that.addFirstPageUrlToChapters(url, firstPageDom, chapters);
+                chapters = that.addFirstPageUrlToWebPages(url, firstPageDom, chapters);
             }
-            chapters = that.cleanChaperListUrls(chapters);
+            chapters = that.cleanWebPageUrls(chapters);
             let chapterUrlsUI = new ChapterUrlsUI(that);
             chapterUrlsUI.populateChapterUrlsTable(chapters);
             if (0 < chapters.length) {
@@ -229,57 +243,57 @@ class Parser {
                 }
                 that.getProgressBar().value = 0;
             }
-            that.chapters = chapters;
+            that.state.setPagesToFetch(chapters);
             chapterUrlsUI.connectButtonHandlers();
         }).catch(function (err) {
             ErrorLog.showErrorMessage(err);
         });
     }
 
-    cleanChaperListUrls(chapters) {
+    cleanWebPageUrls(webPages) {
         let foundUrls = new Set();
-        let isUnique = function(chapterInfo) {
-            let unique = !foundUrls.has(chapterInfo.sourceUrl);
+        let isUnique = function(webPage) {
+            let unique = !foundUrls.has(webPage.sourceUrl);
             if (unique) {
-                foundUrls.add(chapterInfo.sourceUrl);
+                foundUrls.add(webPage.sourceUrl);
             }
             return unique;
         }
 
-        return chapters
+        return webPages
             .map(this.fixupImgurGalleryUrl)
             .filter(isUnique);
     }
 
-    fixupImgurGalleryUrl(chapter) {
-        chapter.sourceUrl = Imgur.fixupImgurGalleryUrl(chapter.sourceUrl);
-        return chapter;
+    fixupImgurGalleryUrl(webPage) {
+        webPage.sourceUrl = Imgur.fixupImgurGalleryUrl(webPage.sourceUrl);
+        return webPage;
     }
 
-    addFirstPageUrlToChapters(url, firstPageDom, chapters) {
-        let present = chapters.find(e => e.sourceUrl === url);
+    addFirstPageUrlToWebPages(url, firstPageDom, webPages) {
+        let present = webPages.find(e => e.sourceUrl === url);
         if (present)
         {
-            return chapters;
+            return webPages;
         } else {
             return [{
                 sourceUrl:  url,
                 title: this.extractTitle(firstPageDom)
-            }].concat(chapters);
+            }].concat(webPages);
         }
     }
 
     onFetchChaptersClicked() {
-        if (0 == this.chapters.length) {
+        if (0 == this.state.webPages.size) {
             ErrorLog.showErrorMessage(chrome.i18n.getMessage("noChaptersFoundAndFetchClicked"));
         } else {
             this.getFetchContentButton().disabled = true;
-            this.fetchChapters();
+            this.fetchWebPages();
         }
     }
 
     fetchContent() {
-        return this.fetchChapters();
+        return this.fetchWebPages();
     }
 
     setUiToShowLoadingProgress(length) {
@@ -288,10 +302,10 @@ class Parser {
         this.getProgressBar().value = 1;
     }
 
-    fetchChapters() {
+    fetchWebPages() {
         let that = this;
 
-        let pagesToFetch = that.chapters.filter(c => c.isIncludeable);
+        let pagesToFetch = this.state.webPages.values(c => c.isIncludeable);
         if (pagesToFetch.length === 0) {
             return Promise.reject(new Error("No chapters found."));
         }
@@ -304,19 +318,19 @@ class Parser {
         that.imageCollector.setCoverImageUrl(CoverImageUI.getCoverImageUrl());
 
         let initialHostName = this.initialHostName();
-        pagesToFetch.forEach(function(chapter) {
+        pagesToFetch.forEach(function(webPage) {
             
             sequence = sequence.then(function () {
-                return that.fetchChapter(chapter.sourceUrl);
-            }).then(function (chapterDom) {
-                chapter.rawDom = chapterDom;
-                let pageParser = that.parserForChapter(initialHostName, chapter);
-                pageParser.removeUnusedElementsToReduceMemoryConsumption(chapterDom);
-                pageParser.updateLoadState(chapter);
-                let content = pageParser.findContent(chapter.rawDom);
+                return that.fetchChapter(webPage.sourceUrl);
+            }).then(function (webPageDom) {
+                webPage.rawDom = webPageDom;
+                let pageParser = that.parserForWebPage(initialHostName, webPage);
+                pageParser.removeUnusedElementsToReduceMemoryConsumption(webPageDom);
+                pageParser.updateLoadState(webPage);
+                let content = pageParser.findContent(webPage.rawDom);
                 if (content == null) {
-                    chapter.isIncludeable = false;
-                    let errorMsg = chrome.i18n.getMessage("errorContentNotFound", [chapter.sourceUrl]);
+                    webPage.isIncludeable = false;
+                    let errorMsg = chrome.i18n.getMessage("errorContentNotFound", [webPage.sourceUrl]);
                     throw new Error(errorMsg);
                 }
                 return pageParser.fetchImagesUsedInDocument(content);
@@ -332,14 +346,14 @@ class Parser {
     }
 
     initialHostName() {
-        return util.extractHostName(this.chapterListUrl);
+        return util.extractHostName(this.state.chapterListUrl);
     }
 
-    parserForChapter(initialNostName, chapter) {
-        if (util.extractHostName(chapter.sourceUrl) === initialNostName) {
+    parserForWebPage(initialNostName, webPage) {
+        if (util.extractHostName(webPage.sourceUrl) === initialNostName) {
             return this;
         }
-        let pageParser = parserFactory.fetch(chapter.sourceUrl, chapter.rawDom);
+        let pageParser = parserFactory.fetch(webPage.sourceUrl, webPage.rawDom);
         if (pageParser === undefined) {
             return this;
         }
@@ -360,8 +374,8 @@ class Parser {
     * default implementation
     * derivied classes can override if DOM has lots of elements not used in epub
     */
-    removeUnusedElementsToReduceMemoryConsumption(chapterDom) {
-        util.removeElements(chapterDom.querySelectorAll("select, iframe"));
+    removeUnusedElementsToReduceMemoryConsumption(webPageDom) {
+        util.removeElements(webPageDom.querySelectorAll("select, iframe"));
     }
 
     // Hook if need to chase hyperlinks in page to get all chapter content
@@ -371,8 +385,8 @@ class Parser {
         });
     }
 
-    updateLoadState(chapter) {
-        chapter.stateColumn.innerText = "Yes";
+    updateLoadState(webPage) {
+        webPage.stateColumn.innerText = "Yes";
         this.getProgressBar().value += 1;
     }
 
@@ -384,13 +398,13 @@ class Parser {
         return document.getElementById("fetchChaptersButton")
     }
 
-    // pack the raw chapter HTML into a zip file (for later manual analysis)
-    packRawChapters() {
-        let that = this;
+    // pack the raw HTML into a zip file (for later manual analysis)
+    packRawWebPages() {
         let zipFile = new JSZip();
-        for (let i = 0; i < that.chapters.length; ++i) {
-            if (that.chapters[i].rawDom != null) {
-                zipFile.file("chapter" + i + ".html", that.chapters[i].rawDom.documentElement.outerHTML, { compression: "DEFLATE" });
+        let i = -1;
+        for (let page of this.state.webPages.values()) {
+            if (page.rawDom != null) {
+                zipFile.file("chapter" + (++i) + ".html", page.rawDom.documentElement.outerHTML, { compression: "DEFLATE" });
             };
         }
         zipFile.generateAsync({ type: "blob" }).then(function(content) {

plugin/js/parsers/BakaTsukiParser.js
@@ -142,7 +142,7 @@ class BakaTsukiParser extends Parser{
     onLoadFirstPage(url, firstPageDom) {
         let that = this;
         that.firstPageDom = firstPageDom;
-        this.chapterListUrl = url;
+        this.state.chapterListUrl = url;
 
         let content = that.findContent(that.firstPageDom).cloneNode(true);
         that.removeUnwantedElementsFromContentElement(content);

plugin/js/parsers/UltimaguilParser.js
@@ -37,12 +37,12 @@ class UltimaguilParser extends Parser {
         document.getElementById("higestResolutionImagesRow").hidden = false; 
     }
 
-    chapterToEpubItems(chapter, epubItemIndex) {
+    webPageToEpubItems(webPage, epubItemIndex) {
         let that = this;
-        let content = that.convertRawDomToContent(chapter);
+        let content = that.convertRawDomToContent(webPage);
         let items = [];
         if (content != null) {
-            items = that.splitContentIntoSections(content, chapter.sourceUrl, epubItemIndex);
+            items = that.splitContentIntoSections(content, webPage.sourceUrl, epubItemIndex);
         }
         return items;
     }

unitTest/UtestEpubPacker.js
@@ -24,10 +24,10 @@ function makePacker() {
 
 function makeEpubItemSupplier(imageCollector) {
     imageCollector = imageCollector || ImageCollector.StubCollector();
-    let chapters = [];
+    let webPages = [];
     for (let i = 0; i < 2; ++i) {
         let title = "Title" + i;
-        chapters.push({
+        webPages.push({
             sourceUrl: "http://dummy.com/" + title,
             title: title,
             isIncludeable: true,
@@ -36,7 +36,7 @@ function makeEpubItemSupplier(imageCollector) {
         });
     }
     let parser = new ArchiveOfOurOwnParser();
-    let epubItems = parser.chaptersToEpubItems(chapters);
+    let epubItems = parser.webPagesToEpubItems(webPages);
     return new EpubItemSupplier(parser, epubItems, imageCollector);
 }
 

unitTest/UtestFanFictionParser.js
@@ -15,7 +15,7 @@ QUnit.test("getChapterUrls", function (assert) {
     let done = assert.async();
     let parser = new FanFictionParser();
     parser.getChapterUrls(loadFanFictionMultiPageDoc()).then(function (chapterUrls) {
-        chapterUrls = parser.cleanChaperListUrls(chapterUrls);
+        chapterUrls = parser.cleanWebPageUrls(chapterUrls);
         assert.equal(chapterUrls.length, 5);
         assert.equal(chapterUrls[0].sourceUrl, "https://www.fanfiction.net/s/1234567/1/WebToEpub");
         assert.equal(chapterUrls[1].sourceUrl, "https://www.fanfiction.net/s/1234567/2/WebToEpub");

unitTest/UtestParser.js
@@ -3,7 +3,7 @@
 
 module("Parser");
 
-QUnit.test("cleanChaperListUrls", function (assert) {
+QUnit.test("cleanWebPageUrls", function (assert) {
     let sample = [
         {sourceUrl: "http://dummy.com/page1.html"},
         {sourceUrl: "https://imgur.com/a123bgh1"},
@@ -14,6 +14,6 @@ QUnit.test("cleanChaperListUrls", function (assert) {
         {sourceUrl: "http://dummy.com/page1.html"},
         {sourceUrl: "https://imgur.com/a123bgh1?grid"}
     ];
-    let actual = new Parser().cleanChaperListUrls(sample);
+    let actual = new Parser().cleanWebPageUrls(sample);
     assert.deepEqual(actual, expected);
 });

unitTest/UtestWuxiaworldParser.js
@@ -31,10 +31,10 @@ function dummyWuxiaDoc() {
 test("removeUnwantedElementsFromContentElement", function (assert) {
     let dom = dummyWuxiaDoc();
     let parser = new WuxiaworldParser();
-    parser.chapters = [
+    parser.setPagesToFetch([
         { sourceUrl: "http://www.wuxiaworld.com/wmw-index/wmw-chapter-1/" },
         { sourceUrl: "http://www.wuxiaworld.com/wmw-index/wmw-chapter-3" }
-    ];
+    ]);
     let body = parser.removeNextAndPreviousChapterHyperlinks(dom.body);
     assert.equal(dom.body.innerHTML,
            "<sup class=\"footnote\"><a href=\"#fn-63064-1\" id=\"fnref-63064-1\" onclick=\"return fdfootnote_show(63064)\">1</a></sup>" +

commit bad7f5324cb8693d73c4e058bc0cbf71132c15b4

Fix https://github.com/dteviot/WebToEpub/issues/143
(More breakage from commit fc17148ec8e58798684cf1ad6968f91a983c0dd7)

plugin/js/ChapterUrlsUI.js
@@ -200,7 +200,7 @@ class ChapterUrlsUI {
         this.setVisibileUI(this.usingTable);
         let input = ChapterUrlsUI.getEditChaptersUrlsInput();
         input.rows = Math.max(this.parser.getPagesToFetch().size, 20);
-        input.value = this.chaptersToHTML(this.parser.getPagesToFetch().values());
+        input.value = this.chaptersToHTML([...this.parser.getPagesToFetch().values()]);
     }
 
     chaptersToHTML(chapters) {