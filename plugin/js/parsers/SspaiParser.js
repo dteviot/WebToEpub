2fe16593fc5e09ba688dbb90d4d0bfe885509ba5
dteviot <dteviot@gmail.com>
2017-11-14 20:06:16 +1300

    Very basic parser for site ssapi.com

---
+++ plugin/js/parsers/SspaiParser.js
@@ -0,0 +1,21 @@
+/*
+  Very basic parser for sspai.com
+*/
+"use strict";
+
+parserFactory.register("sspai.com", function() { return new SspaiParser() });
+
+class SspaiParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let menu = dom.querySelector("body");
+        return Promise.resolve(util.hyperlinksToChapterList(menu));        
+    };
+
+    findContent(dom) {
+        return dom.querySelector("div.wangEditor-txt");
+    };
+}
