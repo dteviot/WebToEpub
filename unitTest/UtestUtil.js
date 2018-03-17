b29dbedec1ffa4b0b6bcc69128e5b085dc4bae28
dteviot <dteviot@gmail.com>
2018-03-18 11:05:48 +1300

    Removed Wuxiaworld Parser's unit tests.
    Rewrite of Wuxiaworld parser for site's changed format (Refer: https://github.com/dteviot/WebToEpub/issues/157) rendered existing unit tests irrelevant.

--- unitTest/UtestUtil.js
+++ unitTest/UtestUtil.js
@@ -3,6 +3,31 @@
 
 module("UtestUtil");
 
+function dummyWuxiaDoc() {
+    let dom = TestUtils.makeDomWithBody(
+        "<p>" +
+            "<a href=\"http://www.wuxiaworld.com/wmw-index/wmw-chapter-1/\">Previous Chapter</a>" +
+            "<span style=\"float: right\"><a href=\"http://www.wuxiaworld.com/wmw-index/wmw-chapter-3\">Next Chapter</a></span>" +
+        "</p>" +
+        "<sup class='footnote'><a href='#fn-63064-1' id='fnref-63064-1' onclick='return fdfootnote_show(63064)'>1</a></sup>" +
+        "<p>" +
+            "<a href=\"http://www.wuxiaworld.com/wmw-index/wmw-chapter-1\">Previous Chapter</a>" +
+            "<span style=\"float: right\"><a href=\"http://www.wuxiaworld.com/wmw-index/wmw-chapter-3/\">Next Chapter</a></span>" +
+        "</p>" +
+        "<ol>" +
+            "<li id=\"fn-63064-1\" tabindex=\"-1\"> Literal Translation pretty much her wannabe escorts/protectors <span class=\"footnotereverse\">" +
+            "<a href=\"#fnref-63064-1\"><img draggable=\"false\" class=\"emoji\" alt=\"...\" src=\"https://s.w.org/images/core/emoji/72x72/21a9.png\"></a></span></li>" +
+        "</ol>" +
+        // this bit is malformed HTML
+        "<hr>Previous Chapter <span style=\"float: right;\"><a title=\"7 Killers - Chapter 2\" href=\"http://www.wuxiaworld.com/wmw-index/wmw-chapter-3\">Next Chapter</a> </span></p>"
+    );
+
+    // Hack, if I don't do this, on Chrome the href value for <a> tags with just a hash is blank.
+    util.setBaseTag("http://wuxiaworld.com/wmw-index.html", dom);
+
+    return dom;
+}
+
 test("removeEmptyDivElements", function (assert) {
     let dom = TestUtils.makeDomWithBody(
         "<div><h1>H1</h1></div>" +
@@ -234,7 +259,6 @@ test("normalizeUrlForCompare", function (assert) {
 
 test("removeEventHandlers", function (assert) {
     let dom = dummyWuxiaDoc();
-    let parser = new WuxiaworldParser();
     let body = util.removeEventHandlers(dom.body);
     let link = dom.getElementById("fnref-63064-1");
     assert.equal(link.outerHTML, "<a href=\"#fn-63064-1\" id=\"fnref-63064-1\">1</a>");
