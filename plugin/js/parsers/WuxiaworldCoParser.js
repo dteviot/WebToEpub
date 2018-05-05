86ca40584917e66260d98aa89a762efa652ed453
dteviot <dteviot@gmail.com>
2018-05-06 10:50:22 +1200

    Parser for www.wuxiaworld.co
    Refer: https://github.com/dteviot/WebToEpub/issues/71#issuecomment-386582058
    Note, not to be confused with Wixiaworld.com.

---
+++ plugin/js/parsers/WuxiaworldCoParser.js
@@ -0,0 +1,33 @@
+/*
+  parser for http://www.wuxiaworld.co
+*/
+"use strict";
+
+parserFactory.register("www.wuxiaworld.co", function() { return new WuxiaworldCoParser() });
+
+class WuxiaworldCoParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let list = dom.querySelector("div#list");
+        return Promise.resolve(util.hyperlinksToChapterList(list));        
+    };
+
+    findContent(dom) {
+        return dom.querySelector("div#content");
+    };
+
+    extractTitle(dom) {
+        return dom.querySelector("div#info h1").textContent.trim();
+    };
+
+    findChapterTitle(dom) {
+        return dom.querySelector("div.bookname h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div#sidebar");
+    }
+}
