70bca0675cc0064e8da1d7eb280286d1963eccad
Hogesyx <hogesyx@xibo.org>
2018-06-08 12:13:01 +0800

    Update LnmtlParser.js

--- plugin/js/parsers/LnmtlParser.js
+++ plugin/js/parsers/LnmtlParser.js
@@ -42,7 +42,7 @@ class LnmtlParser extends Parser {
 
     customRawDomToContentStep(chapter, content) {
         for(let s of content.querySelectorAll("sentence")) {
-            if (!this.userPreferences.removeOriginal.value && s.className === "original") {
+            if (this.userPreferences.removeOriginal.value && s.className === "original") {
                 s.remove();
             } else {
                 let p = s.ownerDocument.createElement("p");
