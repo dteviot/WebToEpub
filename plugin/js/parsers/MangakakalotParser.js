6e4e6f59a6add1c7ac71dab8bec8531b5d255c5e
dteviot <dteviot@gmail.com>
2018-06-13 19:18:22 +1200

    Add MangakakalotParser

---
+++ plugin/js/parsers/MangakakalotParser.js
@@ -0,0 +1,39 @@
+"use strict";
+
+parserFactory.register("mangakakalot.com", function() { return new MangakakalotParser() });
+
+class MangakakalotParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let chaptersElement = dom.querySelector("div#chapter");
+        return Promise.resolve(util.hyperlinksToChapterList(chaptersElement).reverse());
+    };
+
+    findContent(dom) {
+        return dom.querySelector("div#vungdoc");
+    };
+
+    extractTitle(dom) {
+        return dom.querySelector("div.manga-info-top h1").textContent.trim();
+    };
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("ul.manga-info-text a[href*='search_author']");
+        return (authorLabel === null) ? super.extractAuthor(dom) : authorLabel.textContent;
+    };
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.manga-info-pic");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("ul.manga-info-text, div#noidungm")];
+    }
+}
