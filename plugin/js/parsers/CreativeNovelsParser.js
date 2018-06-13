17d47038a0c74f1cfbd54750f8db84bd4eb7902b
dteviot <dteviot@gmail.com>
2018-06-13 19:24:00 +1200

    fantasy-books.live renamed to creativenovels.com

---
+++ plugin/js/parsers/CreativeNovelsParser.js
@@ -0,0 +1,57 @@
+/*
+  https://fantasy-books.live/ was renamed to creativenovels.com
+*/
+"use strict";
+parserFactory.register("creativenovels.com", function() { return new CreativeNovelsParser() });
+class CreativeNovelsParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let chaptersElement = dom.querySelector("div.post_box");
+        return Promise.resolve(util.hyperlinksToChapterList(chaptersElement));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.content");
+    };
+
+    // title of the story  (not to be confused with title of each chapter)
+    extractTitle(dom) {
+        let title = dom.querySelector("meta[property='og:title']");
+        if (title !== null) {
+            return title.getAttribute("content");
+        }
+        return super.extractTitle(dom);
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeElements(element.querySelectorAll("div.team, div.x-donate-1,"+
+            " div.navigation, div.navi, header.entry-header"));
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("header.entry-header h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "header");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        let nodes = [dom.querySelector("div.title").parentElement];
+        let summary = dom.querySelectorAll("div.tabcontent, div.tabcontent1");
+        for(let node of summary) {
+            let clone = node.cloneNode(true);
+            this.makeHiddenNodesVisible(clone);
+            nodes.push(clone);
+        }
+        return nodes;
+    }
+    
+    makeHiddenNodesVisible(node) {
+        node.removeAttribute("style");
+    }
+}
