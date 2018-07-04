826ace0c47ec618dc8598b0aa4dc1f6b85238c49
dteviot <dteviot@gmail.com>
2018-07-04 20:38:29 +1200

    Fetch multiple pages with ToC for ZenithNovelsParser
    https://github.com/dteviot/WebToEpub/issues/187

--- plugin/js/parsers/ZenithNovelsParser.js
+++ plugin/js/parsers/ZenithNovelsParser.js
@@ -7,6 +7,32 @@ class ZenithNovelsParser extends WordpressBaseParser {
         super();
     }
 
+    getChapterUrls(dom) {
+        let chapters = ZenithNovelsParser.extractPartialChapterList(dom);
+        let tocPages = ZenithNovelsParser.getUrlsOfTocPages(dom);
+        return Promise.all(
+            tocPages.map(url => ZenithNovelsParser.fetchPartialChapterList(url))
+        ).then(function (tocFragments) {
+            return tocFragments.reduce((a, c) => a.concat(c), chapters).reverse();
+        });
+    }
+
+    static getUrlsOfTocPages(dom) {
+        return [...dom.querySelectorAll("ul.lcp_paginator a:not(.lcp_nextlink)")]
+            .map(link => link.href);
+    }
+    
+    static fetchPartialChapterList(url) {
+        return HttpClient.wrapFetch(url).then(function (xhr) {
+            return ZenithNovelsParser.extractPartialChapterList(xhr.responseXML);
+        });
+    }
+    
+    static extractPartialChapterList(dom) {
+        let list = dom.querySelector("ul.lcp_catlist");
+        return util.hyperlinksToChapterList(list);
+    }
+    
     findContent(dom) {
         return dom.querySelector("article");
     }
