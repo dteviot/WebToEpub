f0dddc2f7d51652f596a9087928d7bd2f02e1527
David Teviotdale <dteviotdale@pharos.co.nz>
2018-07-18 17:24:26 +1200

    Add information page for ZenithNovelsParser
    See: https://github.com/dteviot/WebToEpub/issues/173

--- plugin/js/parsers/ZenithNovelsParser.js
+++ plugin/js/parsers/ZenithNovelsParser.js
@@ -36,4 +36,20 @@ class ZenithNovelsParser extends WordpressBaseParser {
     findContent(dom) {
         return dom.querySelector("article");
     }
+
+    getInformationEpubItemChildNodes(dom) {
+        let nodes = [];
+        let summary = [...dom.querySelectorAll("div.entry p")];
+        for(let node of summary) {
+            let clone = node.cloneNode(true);
+            this.cleanInformationNode(clone);
+            nodes.push(clone);
+        }
+        return nodes;
+    }
+
+    cleanInformationNode(node) {
+        let toRemove = [...node.querySelectorAll("img")];
+        util.removeElements(toRemove);
+    }
 }
