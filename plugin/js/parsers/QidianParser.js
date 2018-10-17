f1f49d31d970207878e9c5f12507d173a9936a4c
Asif Mahmood <asif.mahmood.1991@gmail.com>
2018-10-17 16:49:28 +0100

    Fixed extractAuthor function to correctly parse Author's name

--- plugin/js/parsers/QidianParser.js
+++ plugin/js/parsers/QidianParser.js
@@ -41,17 +41,10 @@ class QidianParser extends Parser{
     };
 
     extractAuthor(dom) {
-        let element = dom.querySelector("address");
+        let element = dom.querySelector("address p strong");
         if (element !== null) {
-            element = util.getElement(element, "p", p => p.textContent.startsWith("Author"));
-            if (element !== null) {
-                let author = element.textContent;
-                let strong = element.querySelector("strong");
-                if (strong !== null) {
-                    author = author.substring(strong.textContent.length);
-                }
-                return author;
-            }
+            console.log(element.nextSibling.textContent);
+            return element.nextSibling.textContent;
         }
         return super.extractAuthor(dom);
     }
