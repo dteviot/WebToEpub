5badc753ed248efd3a7671b9489bcb4bba7032a7
dteviot <dteviot@gmail.com>
2019-04-07 16:54:07 +1200

    LightNovelsTranslationParser handle multipage ToC

--- plugin/js/parsers/LightNovelsTranslationsParser.js
+++ plugin/js/parsers/LightNovelsTranslationsParser.js
@@ -7,14 +7,77 @@ class LightNovelsTranslationsParser extends WordpressBaseParser{
         super();
     }
 
+    getChapterUrls(dom) {
+        let paginationUrl = this.getLastPaginationUrl(dom);
+        if (paginationUrl === null) {
+            return super.getChapterUrls(dom);
+        }
+        return Promise.resolve(this.getMultiTocPageChapterUrls(dom, paginationUrl));
+    };
+
+    getMultiTocPageChapterUrls(dom, paginationUrl) {
+        let chapters = LightNovelsTranslationsParser.extractPartialChapterList(dom);
+        let tocPages = this.getUrlsOfTocPages(paginationUrl);
+        return Promise.all(
+            tocPages.map(url => LightNovelsTranslationsParser.fetchPartialChapterList(url))
+        ).then(function (tocFragments) {
+            return tocFragments.reduce((a, c) => a.concat(c), chapters).reverse();
+        });
+    }
+
+    getUrlsOfTocPages(paginationUrl) {
+        let urls = [];
+        let maxPage = parseInt(util.extactSubstring(paginationUrl, "/page/", "/"));
+        let index = paginationUrl.indexOf("/page/") + 6;
+        let prefix = paginationUrl.substring(0, index);
+        for(let i = 2; i <= maxPage; ++i) {
+            urls.push(prefix + i +"/");
+        }
+        return urls;
+    }
+
+    getLastPaginationUrl(dom) {
+        let urls = [...dom.querySelectorAll("div.pagination_container a")];
+        return (0 === urls.length) ? null : urls.pop().href;
+    }
+
+    static fetchPartialChapterList(url) {
+        return HttpClient.wrapFetch(url).then(function (xhr) {
+            return LightNovelsTranslationsParser.extractPartialChapterList(xhr.responseXML);
+        });
+    }
+    
+    static extractPartialChapterList(dom) {
+        return [...dom.querySelectorAll("h2.entry-title a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
     fetchChapter(url) {
         let that = this;
-        return HttpClient.wrapFetch(url).then(function (xhr) {
-            let dom = xhr.responseXML
+        return HttpClient.wrapFetch(url).then(
+            xhr => this.resolveBlogEntry(xhr.responseXML)
+        ).then (function (dom) {
             that.strpElementsThatMessUpLinkReplacement(dom);
             let candidates = that.findLinksToReplace(url, dom);
             return that.replaceHyperlinksWithImages(url, dom, candidates, 0);
         });
+    }
+
+    resolveBlogEntry(dom) {
+        let mainLink = this.linkToRealChapter(dom);
+        if (0 === mainLink.length) {
+            return Promise.resolve(dom);
+        } else if (1 === mainLink.length) {
+            return HttpClient.wrapFetch(mainLink[0].href).then(xhr => xhr.responseXML);
+        } else {
+            ErrorLog.log(`${dom.baseURI} may link to multiple chapters.  Only first chapter retreived.  You probably need to replace the Table of Content entry with one for each chatper`);
+            return HttpClient.wrapFetch(mainLink[0].href).then(xhr => xhr.responseXML);
+        }
+    }
+
+    linkToRealChapter(dom) {
+        return [...dom.querySelectorAll("div.entry-content a")]
+            .filter(a => a.textContent.trim().toUpperCase().includes("CLICK HERE TO READ"));
     }
 
     findLinksToReplace(url, dom) {
