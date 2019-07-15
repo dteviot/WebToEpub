cbc9398903035ceaa3bad517cb5c536fd8e2950a
dteviot <dteviot@gmail.com>
2019-07-16 07:40:28 +1200

    Add ListNovelParser
    See: https://github.com/dteviot/WebToEpub/issues/283
    Tested against
    * https://listnovel.com/novel/super-soldier/

---
+++ plugin/js/parsers/ListNovelParser.js
@@ -0,0 +1,28 @@
+"use strict";
+
+parserFactory.register("listnovel.com", () => new ListNovelParser());
+
+class ListNovelParser extends WordpressBaseParser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let chapters = [...dom.querySelectorAll("li.wp-manga-chapter a")]
+            .map(a => util.hyperLinkToChapter(a));
+        return Promise.resolve(chapters.reverse());
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("div.author-content a");
+        return (authorLabel === null) ? super.extractAuthor(dom) : authorLabel.textContent;
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.summary_image");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.summary__content")];
+    }
+}
