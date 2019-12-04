f4221408e09914b1ce288c99dd7d0396abdfbb34
dteviot <dteviot@gmail.com>
2019-12-04 20:57:42 +1300

    Adjust for www.asianhobbyist.com site changes

--- plugin/js/parsers/AsianHobbyistParser.js
+++ plugin/js/parsers/AsianHobbyistParser.js
@@ -8,128 +8,27 @@ class AsianHobbyistParser extends WordpressBaseParser{
     }
 
     getChapterUrls(dom) {
-        let items = [...dom.querySelectorAll("li.su-post a")]
+        let items = [...dom.querySelectorAll("#content .tabs div.wuji-row ul a")]
             .map(a => util.hyperLinkToChapter(a));
-        if (0 < items.length) {
-            return Promise.resolve(items.reverse());
-        }
-        return AsianHobbyistParser.usePostEntriesAsTitles(dom);
+        return Promise.resolve(items);
     };
 
-    static usePostEntriesAsTitles(dom) {
-        let chapters = AsianHobbyistParser.extractPostEntries(dom);
-        let tocPageUrls = AsianHobbyistParser.getUrlsOfTocPages(dom);
-        return Promise.all(
-            tocPageUrls.map(url => AsianHobbyistParser.fetchPartialChapterList(url))
-        ).then(function (tocFragments) {
-            return tocFragments.reduce((a, c) => a.concat(c), chapters).reverse();
-        });
+    extractTitleImpl(dom) {
+        return dom.querySelector(".post-title.entry-title a");
+    };
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "div.code-block, div.osny-nightmode");
+        super.removeUnwantedElementsFromContentElement(element);
     }
 
-    static getUrlsOfTocPages(dom) {
-        return [...dom.querySelectorAll("div.pagenav.clearfix > a")]
-            .map(a => a.href);
+    findChapterTitle(dom) {
+        let crumbs = [...dom.querySelectorAll("ol.breadcrumb li")]
+            .map(li => li.textContent);
+        return (0 < crumbs.length) ? crumbs[crumbs.length - 1] : null;
     }
 
-    static fetchPartialChapterList(url) {
-        return HttpClient.wrapFetch(url).then(function (xhr) {
-            return AsianHobbyistParser.extractPostEntries(xhr.responseXML);
-        });
-    }
-    
-    static extractPostEntries(dom) {
-        return [...dom.querySelectorAll("h2.post-title.entry-title > a")]
-            .map(a => util.hyperLinkToChapter(a));
-    }
-    
-    static findUrlOfFirstPageOfChapter(dom) {
-        let link = dom.querySelector("div.entry-content div.post-embed blockquote a");
-        if (link != null) {
-            return link.href;
-        }
-
-        if (AsianHobbyistParser.isPaged(dom)) {
-            return null;
-        }
-
-        let content = AsianHobbyistParser.cleanedContent(dom);
-        let links = [...content.querySelectorAll("a")];
-        if (0 < links.length) {
-            return links.pop().href;
-        }
-
-        links = [...content.querySelectorAll("p")]
-            .map(p => p.textContent)
-            .filter(util.isUrl);
-        if (0 < links.length) {
-            return links.pop();
-        }
-
-        return null;
-    }
-
-    static cleanedContent(dom) {
-        let toClean = dom.querySelector("div.entry-content");
-        for(let e of toClean.querySelectorAll("div.code-block, div.nnl_container, div.osny-nightmode")) {
-            e.remove();
-        }
-        return toClean;
-    }
-
-    fetchChapter(url) {
-        let firstUrl = url;
-        return HttpClient.wrapFetch(url).then(function (xhr) {
-            let docToFill = Parser.makeEmptyDocForContent();
-            let newDom = xhr.responseXML;
-            let pageUrl = AsianHobbyistParser.findUrlOfFirstPageOfChapter(newDom);
-            return AsianHobbyistParser.fetchPagesOfChapter(
-                (pageUrl === null) ? firstUrl : pageUrl, docToFill
-            );
-        });
-    }
-
-    findContent(dom) {
-        return Parser.findConstrutedContent(dom);
-    }
-
-    static fetchPagesOfChapter(pageUrl, docToFill) {
-        return HttpClient.wrapFetch(pageUrl).then(function (xhr) {
-            let newDom = xhr.responseXML;
-            let content = AsianHobbyistParser.extractContent(newDom);
-            let nextPageUrl = AsianHobbyistParser.findNextPageUrl(newDom);
-            docToFill.content.appendChild(content);
-            if (nextPageUrl === null) {
-                return Promise.resolve(docToFill.dom);
-            } else {
-                AsianHobbyistParser.removeNextPageHyperlink(content, nextPageUrl);
-                return AsianHobbyistParser.fetchPagesOfChapter(nextPageUrl, docToFill);
-            }
-        });
-    }
-
-    static extractContent(dom) {
-        if (AsianHobbyistParser.isPaged(dom)) {
-            return dom.querySelector("div#acp_content");
-        }
-        return AsianHobbyistParser.cleanedContent(dom);
-    }
-
-    static isPaged(dom) {
-        return dom.querySelector("div.acp_wrapper") != null;
-    }
-
-    static findNextPageUrl(dom) {
-        let link = dom.querySelector("li.acp_next_page a");
-        if ((link != null) && (link.parentNode.style.display === "")) {
-            return link.href;
-        }
-        return null;
-    }
-    
-    static removeNextPageHyperlink(content, url) {
-        let checkUrl = util.normalizeUrlForCompare(url);
-        [...content.querySelectorAll("a")]
-            .filter(a => util.normalizeUrlForCompare(a.href) === checkUrl)
-            .forEach(a => a.remove());
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.entry-content");
     }
 }
