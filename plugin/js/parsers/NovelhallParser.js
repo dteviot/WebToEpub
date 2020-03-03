72c33ff95556d11e6d5e5b53ba764176341c59bb
dteviot <dteviot@gmail.com>
2020-03-03 21:49:47 +1300

    Add WebNovelOnlineParser
    Resolves: https://github.com/dteviot/WebToEpub/issues/326

---
+++ plugin/js/parsers/NovelhallParser.js
@@ -0,0 +1,45 @@
+"use strict";
+
+parserFactory.register("novelhall.com", () => new NovelhallParser());
+
+class NovelhallParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    getChapterUrls(dom) {
+        let chapters = [...dom.querySelectorAll("div.book-catalog")]
+            .map(c => [...c.querySelectorAll("a")])
+            .reduce((a, c) => a.length < c.length ? c : a, [])
+            .map(a => util.hyperLinkToChapter(a));
+        return Promise.resolve(chapters);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("article div.entry-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("div.book-info h1");
+    }
+
+    extractAuthor(dom) {
+        let meta = dom.querySelector("meta[property='books:author']");
+        if (meta !== null) {
+            meta = meta.getAttribute("content");
+        }
+        return (meta === null) ? super.extractAuthor(dom) : meta;
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("article div.single-header h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.book-img");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.book-info div.intro")];
+    }
+}
