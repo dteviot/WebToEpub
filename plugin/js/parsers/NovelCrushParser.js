faa62eac24be6422a9b7503b66a5ad3d5023ae67
dteviot <dteviot@gmail.com>
2020-03-15 11:55:22 +1300

    Add NovelCrushParser

---
+++ plugin/js/parsers/NovelCrushParser.js
@@ -0,0 +1,48 @@
+"use strict";
+
+parserFactory.register("novelcrush.com", () => new NovelCrushParser());
+
+class NovelCrushParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let toc = dom.querySelector("div.page-content-listing");
+        return util.hyperlinksToChapterList(toc).reverse();
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.text-left div.reading-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("div.post-title h1");
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("div.author-content a");
+        return (authorLabel === null) ? super.extractAuthor(dom) : authorLabel.textContent;
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("ol.breadcrumb li.active").textContent;
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.summary_image");
+    }
+
+    removeUnusedElementsToReduceMemoryConsumption(chapterDom) {
+        util.removeChildElementsMatchingCss(chapterDom, "ins[data-ad-format]");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.summary__content")];
+    }
+
+    cleanInformationNode(node) {
+        util.removeChildElementsMatchingCss(node, "div.heateor_sss_sharing_container");
+        return node;
+    }
+}
