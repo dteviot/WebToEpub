e6754ed105289a4b1cb2416faf6684a20db80c54
dteviot <dteviot@gmail.com>
2020-07-19 12:40:08 +1200

    Add vipnovel.com
    Resolves: https://github.com/dteviot/WebToEpub/issues/358

---
+++ plugin/js/parsers/VipNovelParser.js
@@ -0,0 +1,42 @@
+"use strict";
+
+parserFactory.register("vipnovel.com", () => new VipNovelParser());
+
+class VipNovelParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("li.wp-manga-chapter a")]
+            .map(a => util.hyperLinkToChapter(a))
+            .reverse();
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.reading-content");
+    }
+
+    extractTitleImpl(dom) {
+        var title = dom.querySelector("div.post-title h3");
+        util.removeChildElementsMatchingCss(title, "span.hot");
+        return title;
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("div.author-content a");
+        return (authorLabel === null) ? super.extractAuthor(dom) : authorLabel.textContent;
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.summary_image");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.summary_content, div.summary__content")];
+    }
+
+    cleanInformationNode(node) {
+        util.removeChildElementsMatchingCss(node, "#init-links");
+    }
+}
