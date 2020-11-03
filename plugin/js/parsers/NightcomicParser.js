9046972bf2487ad13ef0425b19b7a1d3870e7a25
dteviot <dteviot@gmail.com>
2020-11-03 19:31:47 +1300

    Add site https://mangabob.com/

--- plugin/js/parsers/NightcomicParser.js
+++ plugin/js/parsers/NightcomicParser.js
@@ -4,6 +4,7 @@ parserFactory.register("nightcomic.com", function() { return new NightcomicParse
 parserFactory.register("webnovel.live", function() { return new NightcomicParser() });
 parserFactory.register("noveltrench.com", function() { return new NightcomicParser() });
 parserFactory.register("mangasushi.net", function() { return new NightcomicParser() });
+parserFactory.register("mangabob.com", function() { return new NightcomicParser() });
 
 class NightcomicParser extends Parser{
     constructor() {
