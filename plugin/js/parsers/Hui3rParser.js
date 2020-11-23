812d47caa21f11109d89ba40e79f81bb5c3495b5
dteviot <dteviot@gmail.com>
2020-11-23 20:51:59 +1300

    Add site https://hui3r.wordpress.com
    See: https://github.com/dteviot/WebToEpub/issues/424

---
+++ plugin/js/parsers/Hui3rParser.js
@@ -0,0 +1,27 @@
+"use strict";
+
+parserFactory.register("hui3r.wordpress.com", () => new Hui3rParser());
+
+class Hui3rParser extends WordpressBaseParser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("div.single-entry-content ul a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.single-entry-content");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "#jp-post-flair, footer.entry-meta");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector(".entry-title");
+    }
+}
