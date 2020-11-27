98a03d6f8848a9725fb731d71a5ca72b80eafd0e
dteviot <dteviot@gmail.com>
2020-11-27 19:46:17 +1300

    Add site https://idnovel.my.id/
    See: https://github.com/dteviot/WebToEpub/issues/428

---
+++ plugin/js/parsers/IdnovelmyidParser.js
@@ -0,0 +1,43 @@
+"use strict";
+
+parserFactory.register("idnovel.my.id", () => new IdnovelmyidParser());
+
+class IdnovelmyidParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("div.bxcl");
+        return util.hyperlinksToChapterList(menu).reverse();
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.text-left");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("div.infox h1");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "center");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("div.maincontent b");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.thumb");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.desc")];
+    }
+
+    cleanInformationNode(node) {
+        util.removeChildElementsMatchingCss(node, "script");
+    }
+}
