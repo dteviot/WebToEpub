22c5b2ecd9eb40e26a0e082642034c94c70fc24d
dteviot <dteviot@gmail.com>
2021-01-03 16:12:51 +1300

    Add site https://www.xbiquge.cc/
    See: https://github.com/dteviot/WebToEpub/issues/452

---
+++ plugin/js/parsers/XbiqugeParser.js
@@ -0,0 +1,46 @@
+"use strict";
+
+parserFactory.register("xbiquge.cc", () => new XbiqugeParser());
+
+class XbiqugeParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("div#list");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div#content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    extractLanguage() {
+        return "cn";
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("div.bookname h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div#fmimg");
+    }
+
+    async fetchChapter(url) {
+        // site does not tell us gbk is used to encode text
+        let options = { 
+            makeTextDecoder: () => new TextDecoder("gbk") 
+        };
+        return (await HttpClient.wrapFetch(url, options)).responseXML;
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div#info")];
+    }
+}
