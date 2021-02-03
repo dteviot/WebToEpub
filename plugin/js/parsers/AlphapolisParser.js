0a2afa389cc86f287f13fb5505cacdc5d34a91b9
dteviot <dteviot@gmail.com>
2021-02-03 21:26:43 +1300

    Fixed minor issues in gamebeaker's commit
    1. Missing comma in package.json
    2. Minor eslint warnings

--- plugin/js/parsers/AlphapolisParser.js
+++ plugin/js/parsers/AlphapolisParser.js
@@ -5,7 +5,7 @@ class AlphapolisParser extends Parser{
         super();
     }
     async getChapterUrls(dom) {
-		let menu = dom.querySelector("div.episodes");
+        let menu = dom.querySelector("div.episodes");
         return util.hyperlinksToChapterList(menu);
     }
     findContent(dom) {
@@ -14,11 +14,11 @@ class AlphapolisParser extends Parser{
     extractTitleImpl(dom) {
         return dom.querySelector("h2.title");
     }
-extractAuthor(dom) {
-    let authorLink = dom.querySelector("div.author a");
-    return (authorLink === null) ? super.extractAuthor(dom) : authorLink.textContent;
-};
-    extractLanguage(dom) {
+    extractAuthor(dom) {
+        let authorLink = dom.querySelector("div.author a");
+        return (authorLink === null) ? super.extractAuthor(dom) : authorLink.textContent;
+    };
+    extractLanguage() {
         return "jp";
     }
     findChapterTitle(dom) {
@@ -27,7 +27,7 @@ extractAuthor(dom) {
     findCoverImageUrl(dom) {
         return util.getFirstImgSrc(dom, "div.content-info .cover");
     }
-	getInformationEpubItemChildNodes(dom) {
+    getInformationEpubItemChildNodes(dom) {
         return [...dom.querySelectorAll("div.abstract")];
     }
 }
