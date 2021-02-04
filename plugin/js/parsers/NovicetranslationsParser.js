3aab53d36289f0db6b04c49b67bb3665206b325f
dteviot <dteviot@gmail.com>
2021-02-04 20:52:02 +1300

    Simplify NovicetranslationsParser

--- plugin/js/parsers/NovicetranslationsParser.js
+++ plugin/js/parsers/NovicetranslationsParser.js
@@ -1,6 +1,6 @@
 "use strict";
 parserFactory.register("novicetranslations.com", () => new NovicetranslationsParser());
-class NovicetranslationsParser extends Parser{
+class NovicetranslationsParser extends WordpressBaseParser{
     constructor() {
         super();
     }
@@ -8,15 +8,7 @@ class NovicetranslationsParser extends Parser{
         let menu = dom.querySelector("div.entry");
         return util.hyperlinksToChapterList(menu);
     }
-    findContent(dom) {
-        return dom.querySelector("div.entry-content");
-    }
-    extractTitleImpl(dom) {
-        return dom.querySelector("titel");
-    }
-    findChapterTitle(dom) {
-        return dom.querySelector("h2.entry-title");
-    }
+
     findCoverImageUrl(dom) { 
         return util.getFirstImgSrc(dom, "div.wp-block-image");
     }
