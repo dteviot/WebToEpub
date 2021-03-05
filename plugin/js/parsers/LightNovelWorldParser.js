b0166117691282330247154bc8d9355bedf94659
dteviot <dteviot@gmail.com>
2021-03-05 19:43:11 +1300

    Site https://www.lightnovelpub.com renamed
    lightnovelworld.com changed to lightnovelpub.com
    See: https://github.com/dteviot/WebToEpub/pull/486

--- plugin/js/parsers/LightNovelWorldParser.js
+++ plugin/js/parsers/LightNovelWorldParser.js
@@ -1,6 +1,7 @@
 "use strict";
 
 parserFactory.register("lightnovelworld.com", () => new LightNovelWorldParser());
+parserFactory.register("lightnovelpub.com", () => new LightNovelWorldParser());
 
 class LightNovelWorldParser extends Parser{
     constructor() {
