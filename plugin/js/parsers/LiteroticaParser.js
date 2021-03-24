e991c63771d7216c6569e51101bf5442ccb1d65a
Zero <LordZero25@gmail.com>
2021-03-25 06:37:36 +0800

    Fixed Literotica parser

--- plugin/js/parsers/LiteroticaParser.js
+++ plugin/js/parsers/LiteroticaParser.js
@@ -3,9 +3,11 @@
 */
 "use strict";
 
-parserFactory.register("literotica.com", function() { return new LiteroticaParser() });
+parserFactory.register("literotica.com", function () {
+    return new LiteroticaParser();
+});
 
-class LiteroticaParser extends Parser{
+class LiteroticaParser extends Parser {
     constructor() {
         super();
     }
@@ -13,12 +15,16 @@ class LiteroticaParser extends Parser{
     getChapterUrls(dom) {
         let chapters = this.chaptersFromMemberPage(dom);
         return Promise.resolve(chapters);
-    };
+    }
 
     chaptersFromMemberPage(dom) {
-        let links = [...dom.querySelectorAll("td.fc a, div.b-story-list-box h3 a, div.b-story-list h3 a")];
+        let links = [
+            ...dom.querySelectorAll(
+                "td.fc a, div.b-story-list-box h3 a, div.b-story-list h3 a"
+            ),
+        ];
         if (0 < links.length) {
-            return links.map(a => util.hyperLinkToChapter(a));
+            return links.map((a) => util.hyperLinkToChapter(a));
         }
         let content = dom.querySelector("div#content");
         return content === null ? [] : util.hyperlinksToChapterList(content);
@@ -26,32 +32,53 @@ class LiteroticaParser extends Parser{
 
     findContent(dom) {
         return LiteroticaParser.contentForPage(dom);
-    };
+    }
 
     static contentForPage(dom) {
-        return dom.querySelector("div.b-story-body-x");
+        return dom.querySelector("div.aa_ht");
     }
 
     findChapterTitle(dom) {
-        return dom.querySelector("div.b-story-header h1");
+        return dom.querySelector("h1.j_eQ");
     }
 
     fetchChapter(url) {
         let dom = null;
-        return HttpClient.wrapFetch(url).then(function (xhr) {
-            dom = xhr.responseXML;
-            let pageUrls = LiteroticaParser.findUrlsOfAdditionalPagesMakingChapter(url, dom);
-            return Promise.all(pageUrls.map(LiteroticaParser.fetchAdditionalPageContent));
-        }).then(function (fragments) {
-            return LiteroticaParser.assembleChapter(dom, fragments);
-        });
+        return HttpClient.wrapFetch(url)
+            .then(function (xhr) {
+                dom = xhr.responseXML;
+                let pageUrls = LiteroticaParser.findUrlsOfAdditionalPagesMakingChapter(
+                    url,
+                    dom
+                );
+                return Promise.all(
+                    pageUrls.map(LiteroticaParser.fetchAdditionalPageContent)
+                );
+            })
+            .then(function (fragments) {
+                return LiteroticaParser.assembleChapter(dom, fragments);
+            });
     }
 
     static findUrlsOfAdditionalPagesMakingChapter(url, dom) {
-        return [...dom.querySelectorAll("div.b-pager-pages select option")]
-            .map(o => o.textContent)
-            .filter(t => t !== "1")
-            .map(a => `${url}?page=${a}`); 
+        const paginationItems = dom.querySelectorAll("div.l_bH a.l_bJ");
+        const totalPages = parseInt(
+            paginationItems[paginationItems.length - 1].href.split("=")[1]
+        );
+
+        let urls = [];
+        let i = 2;
+        while (i <= totalPages) {
+            urls.push(`${url}?page=${i}`);
+            i++;
+        }
+
+        return urls;
+
+        // return [...dom.querySelectorAll("div.b-pager-pages select option")]
+        //     .map((o) => o.textContent)
+        //     .filter((t) => t !== "1")
+        //     .map((a) => `${url}?page=${a}`);
     }
 
     static fetchAdditionalPageContent(url) {
@@ -62,8 +89,8 @@ class LiteroticaParser extends Parser{
 
     static assembleChapter(dom, fragments) {
         let content = LiteroticaParser.contentForPage(dom);
-        for(let f of fragments.filter(f => f !== null)) {
-            while(0 < f.children.length) {
+        for (let f of fragments.filter((f) => f !== null)) {
+            while (0 < f.children.length) {
                 content.appendChild(f.children[0]);
             }
         }
