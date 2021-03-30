2f0f66508be348166f5775ce5a6897483d307d8f
dteviot <dteviot@gmail.com>
2021-03-30 23:34:58 +1300

    Add site https://novel.naver.com/
    See: https://github.com/dteviot/WebToEpub/issues/495
    Not sure how good this is. I don't know Korean.
    
    Works for
    https://novel.naver.com/webnovel/list.nhn?novelId=933819&page=1

---
+++ plugin/js/parsers/NovelNaverParser.js
@@ -0,0 +1,67 @@
+"use strict";
+
+parserFactory.register("novel.naver.com", () => new NovelNaverParser());
+
+class NovelNaverParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom, chapterUrlsUI) {
+        let chapters = this.extractPartialChapterList(dom);
+        let urlsOfTocPages = this.extractTocPageUrls(dom);
+        return (await Parser.getChaptersFromAllTocPages(chapters, 
+            this.extractPartialChapterList, urlsOfTocPages, chapterUrlsUI)).reverse();
+    }
+
+    extractTocPageUrls(dom) {
+        let tocLinks = [...dom.querySelectorAll("div.paging a:not(.paging_next)")];
+        let pages = tocLinks
+            .map(a => a.textContent);
+        if (pages.length <= 1) {
+            return [];
+        }
+        let max = parseInt(pages.pop());
+        let baseUrl = tocLinks[0].href;
+        baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+
+        // might be more ToC pages than given in paging control
+        // so check against num chapters
+        let volume = dom.querySelector("div.cont_sub > ul.list_type2 li.volumeComment");
+        if (volume != null) {
+            let maxChapters = parseInt(volume.getAttribute("value"));
+            max = Math.floor(maxChapters + 9) / 10;
+        }
+
+        let tocUrls = [];
+        for(let i = 2; i <= max; ++i) {
+            tocUrls.push(baseUrl + i);
+        }
+        return tocUrls;
+    }
+
+    extractPartialChapterList(dom) {
+        let menu = dom.querySelector("div.cont_sub > ul.list_type2");
+        return [...menu.querySelectorAll("a")]
+            .map(a => ({
+                sourceUrl:  a.href,
+                title: a.querySelector("p.subj").textContent.trim()
+            }));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.viewer_container");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h2.book_title");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.section_area_info");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.section_area_info p.dsc")];
+    }
+}
