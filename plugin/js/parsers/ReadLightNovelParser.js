b1cf4ecfb16edf8aad61db2e02f496aaa855e6ac
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2021-04-27 19:56:40 -0600

    Remove more junk from https://readlightnovel.org/

--- plugin/js/parsers/ReadLightNovelParser.js
+++ plugin/js/parsers/ReadLightNovelParser.js
@@ -39,7 +39,7 @@ class ReadLightNovelParser extends Parser {
                 parent = parent.parentNode;
             };
         };
-        
+
         // get the title
         if (panelDiv !== null) {
             let titleDiv = panelDiv.querySelector("div.panel-heading");
@@ -56,7 +56,7 @@ class ReadLightNovelParser extends Parser {
 
     extractAuthor(dom) {
         let that = this;
-        let div = util.getElement(dom, "div", d => (d.className === "novel-detail-item") && 
+        let div = util.getElement(dom, "div", d => (d.className === "novel-detail-item") &&
             (that.novelDetailHeaderName(d) === "Author(s)"));
         if (div !== null) {
             let li = div.querySelector("li");
@@ -66,13 +66,13 @@ class ReadLightNovelParser extends Parser {
         };
         return super.extractAuthor(dom);
     }
- 
+
     novelDetailHeaderName(div) {
         let header = div.querySelector("div.novel-detail-item-header");
         if (header !== null) {
             return header.innerText.trim();
         }
-        return "";    
+        return "";
     }
 
     findCoverImageUrl(dom) {
@@ -89,10 +89,70 @@ class ReadLightNovelParser extends Parser {
     }
 
     removeUnwantedElementsFromContentElement(element) {
+        let firstBr = element.querySelector('br:first-of-type');
+        if(firstBr.nextSibling.data.includes("Chapter") &&
+           firstBr.nextSibling.nextSibling.tagName == "BR") {
+            util.removeElements([firstBr, firstBr.nextSibling,
+                firstBr.nextSibling.nextSibling]);
+        } else {
+            util.log("missing chapter")
+        }
+
+        for(let a of element.querySelectorAll('.adsbyvli')) {
+            let center = a.parentNode;
+            let br2 = center.previousElementSibling;
+            let br1 = br2.previousElementSibling;
+            let br3 = center.nextElementSibling;
+            let br4 = br3.nextElementSibling;
+            let hr = br4.nextElementSibling;
+
+            if(br1.tagName == "BR" &&
+                br2.tagName == "BR" &&
+                br3.tagName == "BR" &&
+                br4.tagName == "BR" &&
+                hr.tagName == "HR" &&
+                center.tagName == "CENTER") {
+                util.removeElements([br1, br2, center, br3, br4, hr]);
+            } else {
+                util.log("incorrect adsbyvli pattern")
+            }
+        }
+
+        for(let small of element.querySelectorAll('.ads-title')) {
+            let br1 = small.previousElementSibling;
+            let br2 = small.nextElementSibling;
+            let center = br2.nextElementSibling;
+            let hr = center.nextElementSibling;
+
+            if(br1.tagName == "BR" &&
+                br2.tagName == "BR" &&
+                hr.tagName == "HR" &&
+                small.tagName == "SMALL" &&
+                center.tagName == "CENTER") {
+                util.removeElements([br1, small, br2, center, hr]);
+            } else {
+                util.log("incorrect ads-title pattern")
+            }
+        }
+
+        for(let s of element.querySelectorAll('center > script')) {
+            let center = s.parentNode;
+            let hr = center.nextElementSibling;
+
+            if(center.tagName == "CENTER") {
+                util.removeElements([center]);
+            } else {
+                util.log("incorrect ads-title pattern")
+            }
+            if(hr.tagName == "HR") {
+                util.removeElements([hr]);
+            }
+
+        }
+
         super.removeUnwantedElementsFromContentElement(element);
         util.removeChildElementsMatchingCss(element, "div.row, " +
-            ".alert, .ads-title, " +
-            "img[src*='/magnify-clip.png'], div.hidden");
+            ".alert, img[src*='/magnify-clip.png'], div.hidden");
         this.removeShareThisLinks(element);
     }
 
