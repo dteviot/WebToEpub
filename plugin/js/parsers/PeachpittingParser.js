1352d04f535cd942e13fa2b877f651a527a83d30
dteviot <dteviot@gmail.com>
2021-04-30 20:48:21 +1200

    Add site https://peachpitting.com
    See: https://github.com/dteviot/WebToEpub/issues/520

---
+++ plugin/js/parsers/PeachpittingParser.js
@@ -0,0 +1,39 @@
+"use strict";
+
+parserFactory.register("peachpitting.com", () => new PeachpittingParser());
+
+class PeachpittingParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("div.pt-cv-wrapper a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div#wtr-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h3.elementor-heading-title");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, ".pp-multiple-authors-wrapper");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("div.post-title h2");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div#primary");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.elementor-text-editor")].slice(1, 2);
+    }
+}
