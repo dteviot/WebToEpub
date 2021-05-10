c3c531e6410e980ccb06fb87593f8cd20f65da8b
dteviot <dteviot@gmail.com>
2021-05-10 22:09:18 +1200

    Add site https://www.readwn.com
    See: https://github.com/dteviot/WebToEpub/issues/529

---
+++ plugin/js/parsers/ReadwnParser.js
@@ -0,0 +1,73 @@
+"use strict";
+
+parserFactory.register("readwn.com", () => new ReadwnParser());
+
+class ReadwnParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom, chapterUrlsUI) {
+        return this.getChapterUrlsFromMultipleTocPages(dom,
+            ReadwnParser.extractPartialChapterList,
+            ReadwnParser.getUrlsOfTocPages,
+            chapterUrlsUI
+        );
+    }
+
+    static getUrlsOfTocPages(dom) {
+        let tocLinks = [...dom.querySelectorAll("ul.pagination li a")]
+            .map(l => new URL(l.href))
+            .filter(l => l.search.includes("page"));
+        let pageIds = tocLinks.map(l => parseInt(l.searchParams.get("page")));
+        let maxPage = Math.max(...pageIds);
+        let baseUrl = tocLinks[0];
+        let urls = [];
+        for (var i = 1; i <= maxPage; ++i) {
+            let params = baseUrl.searchParams;
+            params.set("page", i);
+            baseUrl.search = params.toString();
+            urls.push(baseUrl.href);
+        }
+        return urls;
+    }
+
+    static extractPartialChapterList(dom) {
+        return [...dom.querySelectorAll("ul.chapter-list a")]
+            .map(link => ({
+                sourceUrl:  link.href,
+                title: link.querySelector(".chapter-no").textContent.trim() + ":" +
+                    link.querySelector(".chapter-title").textContent.trim()
+            }));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.chapter-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("div.main-head h1");
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("div.author a");
+        return (authorLabel === null) ? super.extractAuthor(dom) : authorLabel.textContent;
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, ".adsbox");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h2");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "figure.cover");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".summary .content")];
+    }
+}
