0adfd4e9178b6d96072e22b8433b43529a3019ed
dteviot <dteviot@gmail.com>
2021-06-01 21:51:15 +1200

    Add site https://aerialrain.com
    See: https://github.com/dteviot/WebToEpub/issues/540

---
+++ plugin/js/parsers/AerialrainParser.js
@@ -0,0 +1,22 @@
+"use strict";
+
+parserFactory.register("aerialrain.com", () => new AerialrainParser());
+
+class AerialrainParser extends WordpressBaseParser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("div.arconix-toggle-content a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.blog-detail-description");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.blog-detail-description p")];
+    }
+}
