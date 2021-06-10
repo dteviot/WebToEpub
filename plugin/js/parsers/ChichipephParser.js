da3602ce42ebbc68282b61a25adefc8056eb86fb
dteviot <dteviot@gmail.com>
2021-06-10 20:59:34 +1200

    Add site https://chichipeph.com
    See: https://github.com/dteviot/WebToEpub/issues/546

---
+++ plugin/js/parsers/ChichipephParser.js
@@ -0,0 +1,32 @@
+"use strict";
+
+parserFactory.register("chichipeph.com", () => new ChichipephParser());
+
+class ChichipephParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("div.entry-content a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.entry-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("article header h3");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "div.entry-meta");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.entry-content p")]
+            .filter(p => p.querySelector("a") === null);
+    }
+}
