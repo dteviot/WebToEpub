a34c833a3de1e973d523100ff4b687c769c5a947
Dimava <dimava2@ya.ru>
2021-09-15 15:39:32 +0300

    lint RulateParser.js

--- plugin/js/parsers/RulateParser.js
+++ plugin/js/parsers/RulateParser.js
@@ -2,15 +2,15 @@
 
 parserFactory.register("tl.rulate.ru", () => new RulateParser());
 
-class RulateParser extends Parser{
+class RulateParser extends Parser {
     constructor() {
         super();
     }
 
     async getChapterUrls(dom) {
-		let rows = dom.querySelectorAll('.chapter_row');
-		rows = [...rows].filter(row => !row.querySelector('[data-price]'));
-		return rows.map(row => util.hyperLinkToChapter(row.querySelector('.t a')));
+        let rows = dom.querySelectorAll(".chapter_row");
+        rows = [...rows].filter(row => !row.querySelector("[data-price]"));
+        return rows.map(row => util.hyperLinkToChapter(row.querySelector(".t a")));
     }
 
     findContent(dom) {
@@ -31,10 +31,10 @@ class RulateParser extends Parser{
     }
 
     extractDescription(dom) {
-		return dom.querySelector('meta[property="ranobe:description"]').getAttribute('content');
+        return dom.querySelector("meta[property=\"ranobe:description\"]").getAttribute("content");
     }
 
     findCoverImageUrl(dom) {
-        return util.getFirstImgSrc(dom, '#slick-slide00') || util.getFirstImgSrc(dom, '.slick');
+        return util.getFirstImgSrc(dom, "#slick-slide00") || util.getFirstImgSrc(dom, ".slick");
     }
 }
