ba84bc4715cac0962f88be20da428d115367aab2
Dimava <dimava2@ya.ru>
2021-09-24 01:15:16 +0300

    add npm release scripts use as `npm run release` with optional additional args `pre` and `draft` requires [Github CLI (`gh`)](https://github.com/cli/cli)

---
+++ eslint/release.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var fs = require("fs");
+var npm = require("npm-commands");
+var { execSync } = require('child_process');
+
+let version = JSON.parse(fs.readFileSync("plugin/manifest.json", 'utf-8')).version;
+let isDraft = process.argv.includes('draft');
+let isPrerelease = process.argv.includes('pre');
+let commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+let nameVersion = isPrerelease ? `${version}.pre-${commitHash}` : version;
+
+execSync('npm run lint');
+
+let chromeName = `./eslint/WebToEpub.chrome.${version}.zip`;
+let firefoxName = `./eslint/WebToEpub.firefox.${version}.xpi`;
+let chromeCopyName = `./eslint/WebToEpub.chrome.${nameVersion}.zip`
+let firefoxCopyName = `./eslint/WebToEpub.firefox.${nameVersion}.zip`;
+
+fs.copyFileSync(chromeName, chromeCopyName);
+// make firefox version a zip for user to unzip, because unsigned xpis can't be installed
+fs.copyFileSync(firefoxName, firefoxCopyName);
+
+let command = `
+	gh release create
+	${version}${isPrerelease ? '.pre-' + commitHash : ''}
+	--title ${version}${isPrerelease ? '.pre-' + commitHash : ''}
+	${chromeCopyName}
+	${firefoxCopyName}
+	--target ${isPrerelease ? 'ExperimentalTabMode' : 'master'}
+	--notes ""
+	${isPrerelease ? '--prerelease' : ''}
+	${isDraft ? '--draft' : ''}
+`.replace(/\n\t?/g, ' ').trim();
+
+
+console.log(command)
+execSync(command, {stdio: 'inherit'});
