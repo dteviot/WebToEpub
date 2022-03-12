c9bd9d098c10694b7aca4c4f86fc0138c310d11e
dteviot <dteviot@gmail.com>
2022-03-13 11:54:24 +1300

    Add site https://www.wuxiapub.com/
    See: https://github.com/dteviot/WebToEpub/issues/703

--- plugin/js/parsers/ReadwnParser.js
+++ plugin/js/parsers/ReadwnParser.js
@@ -4,6 +4,7 @@ parserFactory.register("readwn.com", () => new ReadwnParser());
 parserFactory.register("novelmt.com", () => new ReadwnParser());
 parserFactory.register("wuxiamtl.com", () => new ReadwnParser());
 parserFactory.register("novelmtl.com", () => new ReadwnParser());
+parserFactory.register("wuxiapub.com", () => new ReadwnParser());
 
 class ReadwnParser extends Parser{
     constructor() {
