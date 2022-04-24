e118f183529f0cf022d4cdc2e6f6d10e52e804f7
dteviot <dteviot@gmail.com>
2022-04-24 14:19:53 +1200

    Add site https://noveltranslatedbyc.blogspot.com/
    See: https://github.com/dteviot/WebToEpub/issues/723

---
+++ plugin/js/parsers/NoveltranslatedbycParser.js
@@ -0,0 +1,37 @@
+"use strict";
+
+parserFactory.register("noveltranslatedbyc.blogspot.com", () => new NoveltranslatedbycParser());
+
+class NoveltranslatedbycParser extends BlogspotParser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("article button")]
+            .map(NoveltranslatedbycParser.buttonToChapter);
+    }
+
+    static buttonToChapter(button) {
+        let onClick = button.getAttribute("onclick");
+        let start = onClick.indexOf("https");
+        let end = onClick.lastIndexOf("'");
+        return {
+            title: button.textContent.trim(),
+            sourceUrl:  onClick.substring(start, end),
+        };
+    }
+
+    findContent(dom) {
+        return dom.querySelector("article center");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "button");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "article");
+    }
+}
