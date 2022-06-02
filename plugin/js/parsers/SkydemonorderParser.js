554cc1e057613ba23cedabc32ee7d2878e2fd4cb
dteviot <dteviot@gmail.com>
2022-06-02 21:03:48 +1200

    Add site https://skydemonorder.com
    See: https://github.com/dteviot/WebToEpub/issues/752

---
+++ plugin/js/parsers/SkydemonorderParser.js
@@ -0,0 +1,35 @@
+"use strict";
+
+parserFactory.register("skydemonorder.com", () => new SkydemonorderParser());
+
+class SkydemonorderParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("div[x-show='expanded'] a")]
+            .map(a => util.hyperLinkToChapter(a))
+            .reverse();
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.prose");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "main");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.prose")];
+    }
+}
