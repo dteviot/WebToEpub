10df6e6c3fb7b8ab20a86a502b4612a980ccaebd
dteviot <dteviot@gmail.com>
2022-09-05 12:10:16 +1200

    Add site https://novelbin.com
    See: https://github.com/dteviot/WebToEpub/issues/796

---
+++ plugin/js/parsers/NovelbinParser.js
@@ -0,0 +1,34 @@
+"use strict";
+
+parserFactory.register("novelbin.com", () => new NovelbinParser());
+
+class NovelbinParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("#list-chapter");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("#chr-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h3.title");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h2");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.book");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".desc-text")];
+    }
+}
