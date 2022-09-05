5ecbbb500c58b79c211d21b8a6b3ecd4ac6aebba
dteviot <dteviot@gmail.com>
2022-09-05 14:03:53 +1200

    Update site https://hostednovel.com
    See: https://github.com/dteviot/WebToEpub/issues/800

--- plugin/js/parsers/HostednovelParser.js
+++ plugin/js/parsers/HostednovelParser.js
@@ -8,25 +8,39 @@ class HostednovelParser extends Parser{
     }
 
     async getChapterUrls(dom, chapterUrlsUI) {
-        let url = new URL(dom.baseURI);
-        url.hash = "";
-        if (!url.pathname.endsWith("/chapters")) {
-            url.pathname += "/chapters";
-        }
-        let urlsOfTocPages = this.extractTocPageUrls(dom, url.toString());
-        let chapters = [];
+        let urlsOfTocPages = this.extractTocPageUrls(dom);
+        let chapters = this.extractPartialChapterList(dom);
         return (await this.getChaptersFromAllTocPages(chapters, 
             this.extractPartialChapterList, urlsOfTocPages, chapterUrlsUI))
             .concat(this.chapterUrlsOnPage(dom));
     }
 
-    extractTocPageUrls(dom, initialTocUrl) {
-        return [...dom.querySelectorAll(".chaptergroup")]
-            .map(s => initialTocUrl + "/" + s.getAttribute("data-id"));
+    extractTocPageUrls(dom) {
+        let urls = [];
+        let lastLink = [...dom.querySelectorAll("nav[aria-label='Pagination'] a")].pop();
+        if (lastLink !== null) {
+            let url = new URL(lastLink.href);
+            let maxPage = parseInt(url.searchParams.get("page"));
+            for(let i = 2; i <= maxPage; ++i) {
+                url.searchParams.set("page", i);
+                urls.push(url.href);
+            }
+        }
+        return urls;
     }
 
     extractPartialChapterList(dom) {
-        return util.hyperlinksToChapterList(dom);
+        return [...dom.querySelectorAll("li.flow-root.my-1 a")]
+            .map(a => ({
+                sourceUrl:  a.href,
+                title: HostednovelParser.formatTitle(a)
+            }));
+    }
+
+    static formatTitle(link) {
+        let div = link.querySelector("div");
+        util.removeChildElementsMatchingCss(div, "span, p");
+        return div.textContent.trim();
     }
 
     chapterUrlsOnPage(dom) {
@@ -35,7 +49,7 @@ class HostednovelParser extends Parser{
     }
 
     findContent(dom) {
-        return dom.querySelector("#chapter");
+        return dom.querySelector("div.fontchanger");
     }
 
     populateUI(dom) {
@@ -51,26 +65,22 @@ class HostednovelParser extends Parser{
 
     removeUnwantedElementsFromContentElement(element) {
         if (this.userPreferences.removeAuthorNotes.value) {
-            util.removeChildElementsMatchingCss(element, ".notes");
+            util.removeChildElementsMatchingCss(element, "div.bg-light-200");
         }
+        util.removeChildElementsMatchingCss(element, "div.adbox");
         super.removeUnwantedElementsFromContentElement(element);
     }
 
     findChapterTitle(dom) {
-        return dom.querySelector("h1");
+        return dom.querySelector("h1 span.fontchanger");
     }
 
-    findCoverImageUrl(dom) {
-        return dom.querySelector("img.cover-image")?.src ?? null;
+    findCoverImageUrl() {
+        // CDN blocks attempt to fetch cover image
+        return null;
     }
 
     getInformationEpubItemChildNodes(dom) {
-        return [this.getInfoCard(dom)]
-            .filter(c => c != null);
-    }
-
-    getInfoCard(dom) {
-        let cards = [...dom.querySelectorAll("div.card-body")]
-        return 1 < cards.length ? cards[1] : null;
+        return [...dom.querySelectorAll(".prose")];
     }
 }
