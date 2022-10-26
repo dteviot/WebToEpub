1b0c2e7cd9ff4b7da7e930a04fb3561688cb7514
Pavan Kumar Sampara <pavan.sampara@gmail.com>
2022-10-26 11:04:47 +0530

    Update CreativeNovelsParser.js

--- plugin/js/parsers/CreativeNovelsParser.js
+++ plugin/js/parsers/CreativeNovelsParser.js
@@ -20,7 +20,7 @@ class CreativeNovelsParser extends Parser{
 
     removeUnwantedElementsFromContentElement(element) {
         util.removeElements(element.querySelectorAll("div.team, div.x-donate-1,"+
-            " div.navigation, div.navi, header.entry-header"));
+            " div.navigation, div.navi, header.entry-header, .support-placement"));
         super.removeUnwantedElementsFromContentElement(element);
     }
 
