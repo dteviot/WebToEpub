2428264533874800a451c5874ee594ccf073506f
dteviot <dteviot@gmail.com>
2023-01-25 22:08:36 +1300

    Add password support for chrysanthemumgarden.com
    Some pages on site require supplying a password before they
    can be downloaded.
    See: https://github.com/dteviot/WebToEpub/issues/907

--- plugin/js/parsers/ChrysanthemumgardenParser.js
+++ plugin/js/parsers/ChrysanthemumgardenParser.js
@@ -7,10 +7,47 @@ class ChrysanthemumgardenParser extends WordpressBaseParser{
         super();
     }
 
+    populateUI(dom) {
+        super.populateUI(dom);
+        document.getElementById("passwordRow").hidden = false; 
+    }
+
     customRawDomToContentStep(chapter, content) {
         let notes = [...chapter.rawDom.querySelectorAll("div.tooltip-container")];
         for(let n of notes) {
             content.appendChild(n);
         }
+    }
+
+    async fetchChapter(url) {
+        let newDom = (await HttpClient.wrapFetch(url)).responseXML;
+        let passwordForm = ChrysanthemumgardenParser.getPasswordForm(newDom);
+        if (passwordForm) {
+            let formData = ChrysanthemumgardenParser.makePasswordFormData(passwordForm);
+            let options = {
+                method: "POST",
+                credentials: "include",
+                body: formData
+            };
+            newDom = (await HttpClient.wrapFetch(url, {fetchOptions: options})).responseXML;
+        }
+        return newDom;
+    }
+
+    static getPasswordForm(dom) {
+        return dom.querySelector("form#password-lock");
+    }
+
+    static makePasswordFormData(form) {
+        let formData = new FormData();
+        let password = document.getElementById("passwordInput").value; 
+        formData.append("site-pass", password);
+        formData.append("nonce-site-pass", ChrysanthemumgardenParser.getInputValue(form, "#nonce-site-pass"));
+        formData.append("_wp_http_referer", ChrysanthemumgardenParser.getInputValue(form, "[name='_wp_http_referer']"));
+        return formData;
+    }
+
+    static getInputValue(form, selector) {
+        return form.querySelector("input" + selector).getAttribute("value");
     }
 }
