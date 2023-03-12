6b813f2247d90f40b5d3986b284aaea1c50ace83
dteviot <dteviot@gmail.com>
2023-03-13 11:42:18 +1300

    Add site https://kemono.party/
    See: https://github.com/dteviot/WebToEpub/issues/932

---
+++ plugin/js/parsers/KemonopartyParser.js
@@ -0,0 +1,43 @@
+"use strict";
+
+parserFactory.register("kemono.party", () => new KemonopartyParser());
+
+class KemonopartyParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom, chapterUrlsUI) {
+        return (await this.getChapterUrlsFromMultipleTocPages(dom,
+            this.extractPartialChapterList,
+            this.getUrlsOfTocPages,
+            chapterUrlsUI
+        )).reverse();
+    };
+
+    getUrlsOfTocPages(dom) {
+        let paginator = dom.querySelector("div.paginator menu");
+        return [...paginator.querySelectorAll("a:not(next)")]
+            .map(link => link.href);
+    }
+    
+    extractPartialChapterList(dom) {
+        let links = [...dom.querySelectorAll(".card-list__items a")];
+        return links.map(l => ({
+            sourceUrl: l.href,
+            title: l.querySelector("header").textContent.trim()
+        }));
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".post__body");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1.post__title");
+    }
+}
