d4b54440590ca265ce59787c94f23c85849835d3
dteviot <dteviot@gmail.com>
2023-03-31 21:35:04 +1300

    Add site https://ckandawrites.online/
    See: https://github.com/dteviot/WebToEpub/issues/950

--- plugin/js/parsers/NoblemtlParser.js
+++ plugin/js/parsers/NoblemtlParser.js
@@ -1,5 +1,6 @@
 "use strict";
 
+parserFactory.register("ckandawrites.online", () => new NoblemtlParser());
 parserFactory.register("daotranslate.com", () => new NoblemtlParser());
 parserFactory.register("genesistls.com", () => new NoblemtlParser());
 parserFactory.register("noblemtl.com", () => new NoblemtlParser());
