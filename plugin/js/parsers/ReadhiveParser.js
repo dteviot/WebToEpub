f190f8b7a7aa1d16552f50ca66888c73642a07a2
dteviot <dteviot@gmail.com>
2023-05-01 11:22:41 +1200

    Fix https://readhive.org/ site changes
    See: https://github.com/dteviot/WebToEpub/issues/962

--- plugin/js/parsers/ReadhiveParser.js
+++ plugin/js/parsers/ReadhiveParser.js
@@ -8,7 +8,8 @@ class ReadhiveParser extends Parser{
     }
 
     async getChapterUrls(dom) {
-        return [...dom.querySelectorAll("div.grid-in-content a")]
+        const tab = this.getTab(dom, "releases"); 
+        return [...tab.querySelectorAll("a")]
             .map(ReadhiveParser.linkToChapter)
             .reverse();
     }
@@ -16,12 +17,18 @@ class ReadhiveParser extends Parser{
     static linkToChapter(link) {
         return ({
             sourceUrl:  link.href,
-            title: link.querySelector("p").textContent.trim(),
+            title: link.querySelector("span.ml-1").textContent.trim(),
         });
     }
 
+    getTab(dom, filter) {
+        return [...dom.querySelectorAll("main div[x-show]")]
+            .filter(d => d.getAttribute("x-show")?.includes(filter))
+            ?.pop();
+    }
+
     findContent(dom) {
-        return [...dom.querySelectorAll("#main  > div.justify-center")][1];
+        return dom.querySelector("main div.justify-center:not([x-data]");
     }
 
     extractTitleImpl(dom) {
@@ -29,14 +36,36 @@ class ReadhiveParser extends Parser{
     }
 
     findChapterTitle(dom) {
-        return dom.querySelector("h1");
+        return dom.querySelector("h1 strong");
+    }
+
+    customRawDomToContentStep(chapter, content) {
+        for(let e of content.querySelectorAll("div")) {
+            let toRemove = [];
+            for(let attr of e.attributes) {
+                if (attr.name.startsWith("@")) {
+                    toRemove.push(attr.name);
+                }
+            }
+            for(let attr of toRemove) {
+                e.removeAttribute(attr);
+            }
+        }
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "div[x-data]");
+        super.removeUnwantedElementsFromContentElement(element);
     }
 
     findCoverImageUrl(dom) {
-        return util.getFirstImgSrc(dom, "div.grid-in-art");
+        return util.getFirstImgSrc(dom, "main");
     }
 
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelectorAll("div.prose p")];
+        const tab = this.getTab(dom, "about"); 
+        return tab == null
+            ? []
+            : [...tab.querySelectorAll("p")];
     }
 }
