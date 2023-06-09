ed00e5fb764023e11be4bda562eb3490506366dc
dteviot <dteviot@gmail.com>
2023-06-09 20:01:19 +1200

    Add site https://wanderinginn.com/
    See: https://github.com/dteviot/WebToEpub/issues/984

---
+++ plugin/js/parsers/WanderinginnParser.js
@@ -0,0 +1,22 @@
+"use strict";
+
+parserFactory.register("wanderinginn.com", () => new WanderinginnParser());
+
+class WanderinginnParser extends WordpressBaseParser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("#table-of-contents a:not(.book-title-num)")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+    
+    extractTitleImpl() {
+        return "The Wandering Inn";
+    }
+    
+    extractAuthor() {
+        return "pirateaba";
+    }    
+}
