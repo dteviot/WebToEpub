d7e21e4dd3fdec4fd00d6505a2fd26f7d7816476
dteviot <dteviot@gmail.com>
2023-06-13 20:25:56 +1200

    Add site https://fullnovel.co/
    See: https://github.com/dteviot/WebToEpub/issues/976

---
+++ plugin/js/parsers/FullnovelParser.js
@@ -0,0 +1,34 @@
+"use strict";
+
+parserFactory.register("fullnovel.co", () => new FullnovelParser());
+
+class FullnovelParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("ul.chapter-list");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".chapter-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "main");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.text-xs")];
+    }
+}
