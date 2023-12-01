6780c30922167628229f0e7bd4056a561800d319
dteviot <dteviot@gmail.com>
2023-12-01 19:34:53 +1300

    Add site https://novellive.com/
    See: https://github.com/dteviot/WebToEpub/issues/1125

--- plugin/js/parsers/FreeWebNovelParser.js
+++ plugin/js/parsers/FreeWebNovelParser.js
@@ -4,6 +4,7 @@ parserFactory.register("freewebnovel.com", () => new FreeWebNovelParser());
 parserFactory.register("bednovel.com", () => new FreeWebNovelParser());
 parserFactory.register("innnovel.com", () => new FreeWebNovelParser());
 parserFactory.register("libread.com", () => new FreeWebNovelParser());
+parserFactory.register("novellive.com", () => new NovelliveParser());
 
 class FreeWebNovelParser extends Parser {
 
@@ -43,5 +44,44 @@ class FreeWebNovelParser extends Parser {
 
     getInformationEpubItemChildNodes(dom) {
         return [...dom.querySelectorAll("div.inner")];
+    }
+}
+
+class NovelliveParser extends FreeWebNovelParser {
+
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom, chapterUrlsUI) {
+        return this.getChapterUrlsFromMultipleTocPages(dom,
+            this.extractPartialChapterList,
+            this.getUrlsOfTocPages,
+            chapterUrlsUI
+        );
+    }
+
+    getUrlsOfTocPages(dom) {
+        // lastUrl should be example https://novellive.com/book/<some-novel-name>/<index>
+        let lastUrl = [...dom.querySelectorAll(".page a.index-container-btn")]?.pop()?.href;
+        let urls = [];
+        if (lastUrl) {
+            let lastTocIndex = lastUrl.lastIndexOf("/");
+            let lastIndexPageName = lastUrl.substring(lastTocIndex + 1);
+            let lastIndex = parseInt(lastIndexPageName);
+            let tocHasMultiplePages = !isNaN(lastIndex);
+            if (tocHasMultiplePages) {
+                let baseUrl = lastUrl.substring(0, lastTocIndex + 1);
+                for (let i = 2; i <= lastIndex; ++i) {
+                    urls.push(baseUrl + i);
+                }
+            }
+        }
+        return urls;
+    }
+
+    extractPartialChapterList(dom) {
+        return [...dom.querySelector(".m-newest2").querySelectorAll("ul li a")]
+            .map(a => util.hyperLinkToChapter(a));
     }
 }
