c03866c4a8e543008f485272e3fe052965d2c13f
dteviot <dteviot@gmail.com>
2023-12-09 11:00:19 +1300

    Add site https://www.shw5.cc/
    See: https://github.com/dteviot/WebToEpub/issues/1099

---
+++ plugin/js/parsers/Shw5Parser.js
@@ -0,0 +1,35 @@
+"use strict";
+
+parserFactory.register("shw5.cc", () => new Shw5Parser());
+
+class Shw5Parser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("div.listmain");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("#chaptercontent");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, "a.ll, a.rr");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, ".cover");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".intro")];
+    }
+}
