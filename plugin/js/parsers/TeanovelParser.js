89a827a3b0247337e2d4a8a9c8106c2789ac2ccb
dteviot <dteviot@gmail.com>
2024-01-14 17:42:13 +1300

    Changes for site https://www.teanovel.com/
    See: https://github.com/dteviot/WebToEpub/issues/1164

--- plugin/js/parsers/TeanovelParser.js
+++ plugin/js/parsers/TeanovelParser.js
@@ -8,39 +8,24 @@ class TeanovelParser extends Parser{
         super();
     }
 
-    async getChapterUrls(dom, chapterUrlsUI) {
+    async getChapterUrls(dom) {
         let storyName = new URL(dom.baseURI).pathname.split("/").pop();
-        let baseUrl = dom.baseURI;
-        let baseTocUrl = `https://www.teanovel.com/api/chapters/${storyName}?slug=${storyName}&orderBy=asc`;
-        let json = (await HttpClient.fetchJson(baseTocUrl)).json;
-        let chapters = TeanovelParser.jsonToChapter(json, baseUrl);
-        chapterUrlsUI.showTocProgress(chapters);
-        let url = TeanovelParser.nextTocPageUrl(baseTocUrl, json);
-        while (url != null) {
-            json = (await HttpClient.fetchJson(url)).json;
-            let partialList = TeanovelParser.jsonToChapter(json, baseUrl);
-            chapterUrlsUI.showTocProgress(partialList);
-            chapters = chapters.concat(partialList);
-            url = TeanovelParser.nextTocPageUrl(baseTocUrl, json);
-        }
-        return chapters;
+        let tocUrl = `https://www.teanovel.com/novel/${storyName}/chapter-list`;
+        let chapterDom = (await HttpClient.fetchHtml(tocUrl)).responseXML;
+        return [...chapterDom.querySelectorAll("a.flex")]
+            .map(a => this.linkToChapter(a))
+            .filter(a => a.title);
     }
 
-    static jsonToChapter(json, baseUrl) {
-        return json.data.map(d => ({
-            sourceUrl: baseUrl + "/" + d.slug, 
-            title: `C${d.order} - ${d.title}`
-        }));
-    }
-
-    static nextTocPageUrl(baseTocUrl, json) {
-        return json.nextId == null
-            ? null
-            : baseTocUrl + "&nextId=" + json.nextId;
+    linkToChapter(link) {
+        return ({
+            sourceUrl:  link.href,
+            title: link.querySelector("p.text-sm")?.innerText?.trim(),
+        });
     }
 
     findContent(dom) {
-        return dom.querySelector("article");
+        return dom.querySelector("div.prose");
     }
 
     extractTitleImpl(dom) {
@@ -50,6 +35,10 @@ class TeanovelParser extends Parser{
     removeUnwantedElementsFromContentElement(element) {
         util.removeChildElementsMatchingCss(element, "flex");
         super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
     }
 
     findCoverImageUrl(dom) {
