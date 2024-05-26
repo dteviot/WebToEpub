291e9a9f581f1513ef6d60ff1e592f398e743567
dteviot <dteviot@gmail.com>
2024-05-26 14:03:47 +1200

    Add site https://novelplex.org/
    See: https://github.com/dteviot/WebToEpub/issues/1328

---
+++ plugin/js/parsers/NovelplexParser.js
@@ -0,0 +1,46 @@
+"use strict";
+
+parserFactory.register("novelplex.org", () => new NovelplexParser());
+
+class NovelplexParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll(".nAP__TOCArea a")]
+            .map(a => this.hyperLinkToChapter(a));
+    }
+
+    hyperLinkToChapter(link) {
+        return ({
+            sourceUrl: link.href,
+            title: link.querySelector("p").textContent
+        });
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".halChap--kontenInner");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector(".headerNovel__wrap h2");
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingCss(element, ".pt_aBody, .pt_aButton");
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector(".halChap h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, ".headerNovel");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".summary_mobile")];
+    }
+}
