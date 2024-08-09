76a15beb2b57c381600699c788fe4e459b1bac2d
dteviot <dteviot@gmail.com>
2024-08-10 10:37:27 +1200

    Add site https://helheimscans.com/
    Was Helscans.org.  Also, site layout changed.
    See: https://github.com/dteviot/WebToEpub/issues/1412

---
+++ plugin/js/parsers/HelheimscansParser.js
@@ -0,0 +1,47 @@
+"use strict";
+
+parserFactory.register("helheimscans.com", () => new HelheimscansParser());
+
+class HelheimscansParser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("#chapters_panel a")]
+            .map(this.linkToChapter)
+            .reverse()
+    }
+
+    linkToChapter(link) {
+        let title = link.querySelector("span").textContent.trim();
+        return ({
+            sourceUrl:  link.href,
+            title: title
+        });
+    }
+
+    findContent(dom) {
+        return dom.querySelector("#pages div.novel-reader");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("title");
+    }
+
+    findCoverImageUrl(dom) {
+        let url = dom.querySelector("div[style^=--photo]");
+        url = url.getAttribute("style").split("(")[1];
+        return url
+            ? url.substring(0, url.length - 1)
+            : null;
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("#expand_content")];
+    }
+}
