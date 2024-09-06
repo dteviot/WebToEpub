6fa96b9012c90df587b1ff6d68771b47ab7a781c
gamebeaker <lukas.klipfel@posteo.de>
2024-09-06 14:48:47 +0200

    add fictionzone.net to MtlarchiveParser.js

--- plugin/js/parsers/MtlarchiveParser.js
+++ plugin/js/parsers/MtlarchiveParser.js
@@ -1,6 +1,7 @@
 "use strict";
 
 parserFactory.register("mtlarchive.com", () => new MtlarchiveParser());
+parserFactory.register("fictionzone.net", () => new MtlarchiveParser());
 
 class MtlarchiveParser extends Parser{
     constructor() {
