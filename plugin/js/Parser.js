3562ce370d9537f4ac3d7f67774de5fe60c97811
gamebeaker <lukas.klipfel@posteo.de>
2024-09-28 23:44:01 +0200

    use dom.title if findChapterTitle()=null

--- plugin/js/Parser.js
+++ plugin/js/Parser.js
@@ -108,12 +108,16 @@ class Parser {
 
     addTitleToContent(webPage, content) {
         let title = this.findChapterTitle(webPage.rawDom, webPage);
+        if (webPage.title == "[placeholder]") {
+            if (title != null) {
+                webPage.title = title.trim();
+            } else {
+                webPage.title = webPage.rawDom.title;
+            }
+        }
         if (title != null) {
             if (title instanceof HTMLElement) {
                 title = title.textContent;
-            }
-            if (webPage.title == "[placeholder]") {
-                webPage.title = title.trim();
             }
             if (!this.titleAlreadyPresent(title, content)) {
                 let titleElement = webPage.rawDom.createElement("h1");
