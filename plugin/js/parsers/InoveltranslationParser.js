3e2de1dfdf26ff2f2c650ad74eb993f8739b3f55
inareous <18087264+Inareous@users.noreply.github.com>
2024-12-25 20:42:41 +0700

    fix: inoveltranslation.com

--- plugin/js/parsers/InoveltranslationParser.js
+++ plugin/js/parsers/InoveltranslationParser.js
@@ -8,38 +8,25 @@ class InoveltranslationParser extends Parser{
     }
 
     async getChapterUrls(dom) {
-        let novelId = dom.baseURI.split("/").pop();
-        let tocUrl = `https://api.inoveltranslation.com/novels/${novelId}/feed?limit=100000`;
-        return (await HttpClient.fetchJson(tocUrl)).json.chapters
-            .sort((a, b) => a.volume === b.volume ? a.chapter - b.chapter : a.volume - b.volume)
-            .map(c => this.jsonChapterToChapter(c));
+        return [...dom.querySelectorAll("section[class^='styles_chapter_list'] div:has(>a):not(:has(> div))")]
+            .map(link => this.linkToChapter(link)).reverse();
     }
 
-    jsonChapterToChapter(jsonChapter) {
-        // return wrong hostname, to avoid additional parserFactory registration issues
-        // fixup in fetchChapter()
+    linkToChapter(link) {
+        let a = link.querySelector("a");
+
         return ({
-            title: this.titleFromJson(jsonChapter),
-            sourceUrl: `https://inoveltranslation.com/chapters/${jsonChapter.id}`
+            sourceUrl: a.href,
+            title: a.textContent,
         });
     }
 
-    titleFromJson(jsonChapter) {
-        let title = "";
-        title += util.isNullOrEmpty(jsonChapter.volume)
-            ? ""
-            : `Vol. ${jsonChapter.volume} - `;
-        title += util.isNullOrEmpty(jsonChapter.chapter)
-            ? ""
-            : `Ch. ${jsonChapter.chapter} - `;
-        title += util.isNullOrEmpty(jsonChapter.title)
-            ? ""
-            : jsonChapter.title;
-        return title;
+    findContent(dom) {
+        return dom.querySelector("section[data-sentry-component='RichText']");
     }
 
-    findContent(dom) {
-        return Parser.findConstrutedContent(dom);
+    findChapterTitle(dom) {
+        return dom.querySelector("h1");
     }
 
     extractTitleImpl(dom) {
@@ -50,46 +37,7 @@ class InoveltranslationParser extends Parser{
         return util.getFirstImgSrc(dom, "article");
     }
 
-    async fetchChapter(url) {
-        let apiUrl = url.replace("inoveltranslation.com", "api.inoveltranslation.com");
-        let json = (await HttpClient.fetchJson(apiUrl)).json;
-        let newDoc = Parser.makeEmptyDocForContent(url);
-
-        this.appendElement(newDoc, "h1", this.titleFromJson(json));
-        this.appendParagraphs(newDoc, json.content);
-
-        let notes = (await this.fetchNotes(url));
-        if (notes !== null) {
-            this.appendElement(newDoc, "h3", "Notes");
-            this.appendParagraphs(newDoc, notes);
-        }
-        return newDoc.dom; 
-    }
-
-    appendParagraphs(newDoc, content) {
-        let paragraphs = content.split("\n\n");
-        for(let text of paragraphs) {
-            this.appendElement(newDoc, "p", text);
-        }
-    }
-
-    appendElement(newDoc, tag, text) {
-        let element = newDoc.dom.createElement(tag);
-        element.textContent = text;
-        newDoc.content.appendChild(element);
-    }
-
-    async fetchNotes(url) {
-        let dom = (await HttpClient.wrapFetch(url)).responseXML;
-        let script = dom.querySelector("script#__NEXT_DATA__");
-        if (script === null) {
-            return null;
-        }
-        let json = JSON.parse(script.innerHTML);
-        return json?.props?.pageProps?.chapter?.notes ?? null;
-    }
-
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelectorAll("p.chakra-text")];
+        return [...dom.querySelectorAll("section[class^='styles_details_container'] dl:last-child")];
     }
 }
