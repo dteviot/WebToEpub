95ef0556d08301efa35223fbf3217bd1897709cc
dteviot <dteviot@gmail.com>
2025-01-13 18:55:49 +1300

    Add site https://www.brittanypage43.com/
    See: https://github.com/dteviot/WebToEpub/issues/1624

---
+++ plugin/js/parsers/Brittanypage43Parser.js
@@ -0,0 +1,41 @@
+"use strict";
+
+parserFactory.register("brittanypage43.com", () => new Brittanypage43Parser());
+
+class Brittanypage43Parser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll("a.post-card-content-link")]
+            .map(this.linkToChapter).reverse();
+    }
+
+    linkToChapter(link) {
+        return {
+            sourceUrl:  link.href,
+            title: link.querySelector(".post-card-title").textContent.trim()
+        };
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".gh-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector(".post-card-large header h2");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector(".article-title");
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, ".post-card-image-link");
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".post-card-excerpt")];
+    }
+}
