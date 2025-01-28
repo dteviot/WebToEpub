51f65c7cc7edd6d8e9d7e0b8016d84250439c05c
dteviot <dteviot@gmail.com>
2025-01-28 14:48:48 +1300

    Add site https://www.novel543.com/
    See: https://github.com/dteviot/WebToEpub/issues/1654

---
+++ plugin/js/parsers/Novel543Parser.js
@@ -0,0 +1,53 @@
+"use strict";
+
+parserFactory.register("novel543.com", () => new Novel543Parser());
+
+class Novel543Parser extends Parser{
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let tocUrl = dom.baseURI + "dir";
+        let nextDom = (await HttpClient.wrapFetch(tocUrl)).responseXML;
+        let menu = nextDom.querySelector("div.chaplist ul:nth-of-type(2)");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.chapter-content");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1.title");
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("span.author");
+        return authorLabel?.textContent ?? super.extractAuthor(dom);
+    }
+
+    extractLanguage() {
+        return "zh";
+    }
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, "div.cover");
+    }
+
+    async fetchChapter(url) {
+        return this.walkPagesOfChapter(url, this.moreChapterTextUrl);
+    }
+
+    moreChapterTextUrl(dom) {
+        let has2underscores = (s) => ((s.match(/_/g) || []).length === 2);
+        let nextUrl = [...dom.querySelectorAll(".foot-nav a")].pop();
+        return ((nextUrl != null) && has2underscores(nextUrl.href))
+            ? nextUrl.href
+            : null;
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll("div.intro")];
+    }
+}
