8ed3e0589f3d31bf075176e1805ce409edf23367
gamebeaker <Play4freeNew@gmx.de>
2025-03-30 21:25:21 +0200

    update MvlempyrParser

--- plugin/js/parsers/MvlempyrParser.js
+++ plugin/js/parsers/MvlempyrParser.js
@@ -5,21 +5,27 @@ parserFactory.register("mvlempyr.com", () => new MvlempyrParser());
 class MvlempyrParser extends Parser {
     constructor() {
         super();
+        this.minimumThrottle = 1000;
     }
 
     async getChapterUrls(dom) {
         let imgLink = dom.querySelector("div.novel-image-wrapper img").src;
         let slug = imgLink.split("/").pop().split(".")[0];
-        let chapterCount = parseInt(dom.querySelector("div#chapter-count").textContent);
-        let chapterTitles = [...dom.querySelectorAll("div.chapter-item h3")].map((el) => el.textContent.replace(/^\d+\.\s*/, "")); 
-
+        let chapterCount = parseInt(dom.querySelector("div#chapter-count").textContent)?parseInt(dom.querySelector("div#chapter-count").textContent):-1;
+        
+        if (chapterCount == -1) {
+            let regex = new RegExp("numberOfChapters.*?,");
+            let regex2 = new RegExp("[0-9]+");
+            let script = [...dom.scripts].map(a => a.outerHTML);
+            chapterCount = parseInt(script.filter(a => a.match(regex))?.[0].match(regex)?.[0].match(regex2)?.[0]);
+        }
         let chapterList = [];
 
         for (let i = 1; i <= chapterCount; i++) {
             let link = `https://www.mvlempyr.com/chapter/${slug}-${i}`;
             chapterList.push({
                 sourceUrl: link,
-                title: chapterTitles[i-1],
+                title: `[placeholder]`,
             });
         }
         return chapterList;
