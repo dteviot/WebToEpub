fd772601fd9707c607de44f1742ff0e4e4eeec46
crybx <crybx@users.noreply.github.com>
2025-05-10 21:15:58 -0400

    Add site https://novelingua.com/

---
+++ plugin/js/parsers/NovelinguaParser.js
@@ -0,0 +1,61 @@
+"use strict";
+
+parserFactory.register("novelingua.com", function() { return new NovelinguaParser() });
+
+class NovelinguaParser extends Parser {
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll(".pagelayer-text-holder a")]
+            .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".entry-content .pagelayer-text") ||
+            dom.querySelector(".entry-content .pagelayer-text-holder") ||
+            dom.querySelector(".entry-content") ||
+            dom.querySelector("article");
+    };
+
+    customRawDomToContentStep(chapter, content) {
+        content.querySelectorAll("*").forEach(element => {
+            element.removeAttribute("dir");
+            util.replaceSemanticInlineStylesWithTags(element, true);
+            if (element.tagName === "B" && element.hasAttribute("id")) {
+                element.removeAttribute("id");
+            }
+        });
+    }
+
+    // title of the story (not title of each chapter)
+    extractTitleImpl(dom) {
+        return dom.querySelector(".pagelayer-heading-holder h2");
+    }
+
+    findChapterTitle(dom) {
+        let canonical = dom.querySelector("link[rel='canonical']").href.split("/");
+        let title = canonical.pop();
+        if (title === "") {
+            title = canonical.pop();
+        }
+        title = title.split("-").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+        return title;
+    }
+
+    findCoverImageUrl(dom) {
+        let img = dom.querySelector("img.pagelayer-img");
+        return (img === null) ? img : img.src;
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeElements(element.querySelectorAll("style, .pagelayer-btn-holder, .pagelayer-share"));
+        super.removeUnwantedElementsFromContentElement(element);
+    };
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".entry-content .pagelayer-text-holder")];
+    }
+}
+
