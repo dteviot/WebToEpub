8e8fc486801c9f3344f7ccd30aa10450f4c72771
dteviot <dteviot@gmail.com>
2025-06-26 20:06:37 +1200

    Improve chapter titles for https://www.stellarrealm.net/
    Add chapter number, and title if available

--- plugin/js/parsers/StellarRealmParser.js
+++ plugin/js/parsers/StellarRealmParser.js
@@ -78,7 +78,11 @@ class StellarRealmParser extends Parser{
     buildChapter(json, url) {
         let newDoc = Parser.makeEmptyDocForContent(url);
         let title = newDoc.dom.createElement("h1");
-        title.textContent = json.chapter.title?json.chapter.title:"Chapter "+json.chapter.number;
+        let titleText = "Chapter "+json.chapter.number;
+        if (!util.isNullOrEmpty(json.chapter.title)) {
+            titleText += ": " + json.chapter.title;
+        }
+        title.textContent = titleText;
         newDoc.content.appendChild(title);
         let content = new DOMParser().parseFromString(json.chapter.content, "text/html");
         for(let n of [...content.body.childNodes]) {
