6c978d6dfdee1b3cd3fd4c045fd071b47ca13b0a
dteviot <dteviot@gmail.com>
2025-07-28 11:04:17 +1200

    Fixes for https://www.lanry.space/
    1. Adding Chapter number to titles.
    See: https://github.com/dteviot/WebToEpub/issues/1989
    
    2. Works for URLs  ending in "/Chapter"
    See: https://github.com/dteviot/WebToEpub/issues/2055

--- plugin/js/parsers/LanrySpaceParser.js
+++ plugin/js/parsers/LanrySpaceParser.js
@@ -9,12 +9,8 @@ class LanrySpaceParser extends Parser{
     }
 
     async getChapterUrls(dom) {
-        // eslint-disable-next-line
-        let regex = new RegExp("\/novels\/.+");
-        let slug = "&slug=eq."+dom.baseURI.match(regex)?.[0].slice(8);
-        let apikey = "&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZ2toaXBhc3hxeGl0d2xrdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMzE4MzMsImV4cCI6MjA0NTgwNzgzM30.mHBd2yrRm934yPGy4pui3p7cW4FxfIf6yxh7b2TpUA8";
-        let apibasetoc = "https://vkgkhipasxqxitwlktwz.supabase.co/rest/v1/novels?select=*%2Cchapter_unlocks%21left%28chapter_number%2Cprofile_id%29%2Ccategories%3Acategories_on_novels%28category%3Acategory_id%28id%2Cname%2Ccreated_at%2Cupdated_at%29%29%2Ctags%3Atags_on_novels%21left%28novel_id%2Ctag_id%2Ccreated_at%2Ctag%3Atag_id%28id%2Cname%2Cdescription%29%29%2Cchapters%28id%2Ctitle%2Ccreated_at%2Cchapter_number%2Cpart_number%2Cpublish_at%2Ccoins%2Cvolume_id%2Cage_rating%29"
-        let bookinfo = (await HttpClient.fetchJson(apibasetoc+slug+apikey)).json[0];
+        let slug = this.getSlug(dom);
+        let bookinfo = await this.fetchBookinfo(dom);
         this.bookid = bookinfo.id;
         let unlocked = new Set()
         bookinfo.chapter_unlocks.map(a => unlocked.add(a.chapter_number));
@@ -30,6 +26,23 @@ class LanrySpaceParser extends Parser{
         return chapterlist;
     }
 
+    async fetchBookinfo(dom) {
+        let slug = this.getSlug(dom);
+        let apikey = "&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZ2toaXBhc3hxeGl0d2xrdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMzE4MzMsImV4cCI6MjA0NTgwNzgzM30.mHBd2yrRm934yPGy4pui3p7cW4FxfIf6yxh7b2TpUA8";
+        let apibasetoc = "https://vkgkhipasxqxitwlktwz.supabase.co/rest/v1/novels?select=*%2Cchapter_unlocks%21left%28chapter_number%2Cprofile_id%29%2Ccategories%3Acategories_on_novels%28category%3Acategory_id%28id%2Cname%2Ccreated_at%2Cupdated_at%29%29%2Ctags%3Atags_on_novels%21left%28novel_id%2Ctag_id%2Ccreated_at%2Ctag%3Atag_id%28id%2Cname%2Cdescription%29%29%2Cchapters%28id%2Ctitle%2Ccreated_at%2Cchapter_number%2Cpart_number%2Cpublish_at%2Ccoins%2Cvolume_id%2Cage_rating%29"
+        return (await HttpClient.fetchJson(apibasetoc+slug+apikey)).json[0];
+    }
+
+    getSlug(dom) {
+        // eslint-disable-next-line
+        let regex = new RegExp("\/novels\/.+");
+        let slug = "&slug=eq."+dom.baseURI.match(regex)?.[0].slice(8);
+        const suffix = "/chapters";
+        return slug.endsWith(suffix)
+            ? slug.substring(0, slug.length - suffix.length)
+            : slug;
+    }
+
     sourceURL(a, slug){
         if (a.part_number == null) {
             return "https://www.lanry.space/novels/"+slug+"/c" + a.chapter_number;
@@ -39,20 +52,16 @@ class LanrySpaceParser extends Parser{
     }
 
     chtitle(a){
-        if (a.part_number == null) {
-            return a.title != ""?a.title:"Ch."+a.chapter_number;
-        } else {
-            return a.title != ""?a.title:"Ch."+a.chapter_number + "-" + a.part_number;
-        }
+        let chapNum = (a.part_number == null)
+            ? `Ch. ${a.chapter_number}`
+            : `Ch. ${a.chapter_number}-${a.part_number}`;
+        return (a.title != "")
+            ? chapNum + " " + a.title
+            : chapNum;
     }
     
     async loadEpubMetaInfo(dom){
-        // eslint-disable-next-line
-        let regex = new RegExp("\/novels\/.+");
-        let slug = "&slug=eq."+dom.baseURI.match(regex)?.[0].slice(8);
-        let apikey = "&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZ2toaXBhc3hxeGl0d2xrdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMzE4MzMsImV4cCI6MjA0NTgwNzgzM30.mHBd2yrRm934yPGy4pui3p7cW4FxfIf6yxh7b2TpUA8";
-        let apibasetoc = "https://vkgkhipasxqxitwlktwz.supabase.co/rest/v1/novels?select=*%2Cchapter_unlocks%21left%28chapter_number%2Cprofile_id%29%2Ccategories%3Acategories_on_novels%28category%3Acategory_id%28id%2Cname%2Ccreated_at%2Cupdated_at%29%29%2Ctags%3Atags_on_novels%21left%28novel_id%2Ctag_id%2Ccreated_at%2Ctag%3Atag_id%28id%2Cname%2Cdescription%29%29%2Cchapters%28id%2Ctitle%2Ccreated_at%2Cchapter_number%2Cpart_number%2Cpublish_at%2Ccoins%2Cvolume_id%2Cage_rating%29"
-        let bookinfo = (await HttpClient.fetchJson(apibasetoc+slug+apikey)).json[0];
+        let bookinfo = await this.fetchBookinfo(dom);
         this.title = bookinfo.title;
         this.author = bookinfo.author;
         this.tags = bookinfo.tags.map(a => a.tag.name);
@@ -112,14 +121,7 @@ class LanrySpaceParser extends Parser{
     buildChapter(json, url) {
         let newDoc = Parser.makeEmptyDocForContent(url);
         let title = newDoc.dom.createElement("h1");
-        if (json.title!="") {
-            title.textContent = json.title;
-        }else{
-            // eslint-disable-next-line
-            let regex = new RegExp("\/c.+");
-            let chapterid = url.match(regex)[0].slice(2);
-            title.textContent = "Ch." + chapterid;
-        }
+        title.textContent = this.chtitle(json);
         newDoc.content.appendChild(title);
         let text = json.content.replace("\n\n", "\n");
         text = text.split("\n");
