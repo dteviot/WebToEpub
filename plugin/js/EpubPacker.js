6e94af455b7aa5729045addbbfc49906849e7139
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-11-07 15:58:14 -0500

    make sure that when epub version 3 is generated we prefix the ncx id just like we do the opf id

--- plugin/js/EpubPacker.js
+++ plugin/js/EpubPacker.js
@@ -274,7 +274,7 @@ class EpubPacker {
     }
 
     populateHead(head, ns, depth) {
-        this.appendMetaContent(head, ns, "dtb:uid", this.metaInfo.uuid);
+        this.appendMetaContent(head, ns, "dtb:uid", (this.version === EpubPacker.EPUB_VERSION_3 ? "uri:" : "") + this.metaInfo.uuid);
         this.appendMetaContent(head, ns, "dtb:depth", (depth < 2) ? "2" : depth);
         this.appendMetaContent(head, ns, "dtb:totalPageCount", "0");
         this.appendMetaContent(head, ns, "dtb:maxPageNumber", "0");
