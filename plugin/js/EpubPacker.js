b641d1dd1ace6689df0f3091abac7367df8f3ea9
Peter Kaufman <PeterJamesKaufman@gmail.com>
2025-11-08 15:39:20 -0500

    hardcode the value of the title for the epub to Cover

--- plugin/js/EpubPacker.js
+++ plugin/js/EpubPacker.js
@@ -345,8 +345,7 @@ class EpubPacker {
             file.packInEpub(zipWriter, this.emptyDocFactory, this.contentValidator);
         }
         if (epubItemSupplier.hasCoverImageFile()) {
-            let coverFileDefaultTitle = this.version === EpubPacker.EPUB_VERSION_3 ? this.metaInfo.title : "";
-            let fileContent = epubItemSupplier.makeCoverImageXhtmlFile(this.emptyDocFactory, coverFileDefaultTitle);
+            let fileContent = epubItemSupplier.makeCoverImageXhtmlFile(this.emptyDocFactory, "Cover");
             zipWriter.add(EpubPacker.coverImageXhtmlHref(), new zip.TextReader(fileContent));
         }
     }
