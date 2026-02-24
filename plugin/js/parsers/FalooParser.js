b7e4b559ef84d864adcd42914c42f9cb8ad939cb
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-02-24 22:15:55 +0100

    Added content, title, lang, tags, desc, chapTitle, and cover

--- plugin/js/parsers/FalooParser.js
+++ plugin/js/parsers/FalooParser.js
@@ -1,6 +1,6 @@
 "use strict";
 
-parserFactory.register("faloo.com", () => new FalooParser());
+parserFactory.register("b.faloo.com", () => new FalooParser());
 
 class FalooParser extends Parser {
     constructor() {
@@ -10,5 +10,50 @@ class FalooParser extends Parser {
     async getChapterUrls(dom) {
         return [...dom.querySelectorAll("div.DivTable div div a")]
             .map(a => util.hyperLinkToChapter(a));
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".noveContent");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("#novelName");
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("a.fs14");
+        return authorLabel?.textContent ?? super.extractAuthor(dom);
+    }
+
+    extractLanguage() {
+        return "Zh-CN";
+    }
+
+    extractSubject(dom) {
+        let tags = [...dom.querySelectorAll("div.T-R-T-B2-Box1 a")];
+        return tags.map(e => e.textContent.trim()).join(", ");
+    }
+
+    extractDescription(dom) {
+        return dom.querySelector(".T-L-T-C-Box1").textContent.trim();
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector(".c_l_title h1");
+    }
+
+    findCoverImageUrl(dom) {
+        return dom.querySelector(".imgcss").src;
+    }
+
+    async fetchChapter(url) {
+        // Site might be in gb2312
+        return (await HttpClient.wrapFetch(url, this.makeOptions())).responseXML;
+    }
+
+    makeOptions() {
+        return ({
+            makeTextDecoder: () => new TextDecoder("gb2312")
+        });
     }
 }
