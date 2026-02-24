10d199f7fee8d4fae66f5bd4253a49457d70a8db
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-02-25 00:17:42 +0100

    Added throttles, only get free chapters, no handle for VIP chapters

--- plugin/js/parsers/FalooParser.js
+++ plugin/js/parsers/FalooParser.js
@@ -5,14 +5,29 @@ parserFactory.register("b.faloo.com", () => new FalooParser());
 class FalooParser extends Parser {
     constructor() {
         super();
+
+        //Haven't tried anything between 1000 -> 3000.
+        //1000 and under aren't safe.
+        //3000 is safe.
+        this.minimumThrottle = 3000;
     }
 
     async getChapterUrls(dom) {
-        return [...dom.querySelectorAll("div.DivTable div div a")]
-            .map(a => util.hyperLinkToChapter(a));
+        // Only free chapters
+        let menu = dom.querySelector("div.DivTable:nth-child(3)");
+        return util.hyperlinksToChapterList(menu);
+
+        // Get all chapters, including VIP chapters
+        // return [...dom.querySelectorAll("div.DivTable div div a")]
+        //     .map(a => util.hyperLinkToChapter(a));
     }
 
     findContent(dom) {
+        // Need to handle VIP chapters
+        // if (dom.querySelector(".c_c1").textContent == "您还没有登录，请登录后在继续阅读本部小说!") {
+        //     // No content, VIP chapter, need to login
+        // }
+
         return dom.querySelector(".noveContent");
     }
 
@@ -47,7 +62,6 @@ class FalooParser extends Parser {
     }
 
     async fetchChapter(url) {
-        // Site might be in gb2312
         return (await HttpClient.wrapFetch(url, this.makeOptions())).responseXML;
     }
 
