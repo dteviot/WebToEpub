9bd9d057c6a8a6e3ebdd2d4959f32b6385deb9a9
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2026-02-26 23:50:14 +0530

    Fix formatting issues in EpubMetaInfo.js

--- plugin/js/EpubMetaInfo.js
+++ plugin/js/EpubMetaInfo.js
@@ -35,159 +35,159 @@ class EpubMetaInfo {
     }
 
     static getDefaultStyleSheet() {
-        return ""+
+        return "" +
         // Style for svg images. I got this from BTE-Gen epunbs. Works nicely.
-        "div.svg_outer {\r"+
-        "   display: block;\r"+
-        "   margin-bottom: 0;\r"+
-        "   margin-left: 0;\r"+
-        "   margin-right: 0;\r"+
-        "   margin-top: 0;\r"+
-        "   padding-bottom: 0;\r"+
-        "   padding-left: 0;\r"+
-        "   padding-right: 0;\r"+
-        "   padding-top: 0;\r"+
-        "   text-align: left;\r"+
-        "}\r"+
-        "div.svg_inner {\r"+
-        "   display: block;\r"+
-        "   text-align: center;\r"+
-        "}\r"+
+        "div.svg_outer {\r" +
+        "   display: block;\r" +
+        "   margin-bottom: 0;\r" +
+        "   margin-left: 0;\r" +
+        "   margin-right: 0;\r" +
+        "   margin-top: 0;\r" +
+        "   padding-bottom: 0;\r" +
+        "   padding-left: 0;\r" +
+        "   padding-right: 0;\r" +
+        "   padding-top: 0;\r" +
+        "   text-align: left;\r" +
+        "}\r" +
+        "div.svg_inner {\r" +
+        "   display: block;\r" +
+        "   text-align: center;\r" +
+        "}\r" +
 
         // Centered headings and some margin to make sure it's not too close to the content.
-        "h1, h2 {\r"+
-        "   text-align: center;\r"+
-        "   margin-bottom: 10%;\r"+
-        "   margin-top: 10%;\r"+
-        "}\r"+
-        "h3, h4, h5, h6 {\r"+
-        "   text-align: center;\r"+
-        "   margin-bottom: 15%;\r"+
-        "   margin-top: 10%;\r"+
-        "}\r"+
+        "h1, h2 {\r" +
+        "   text-align: center;\r" +
+        "   margin-bottom: 10%;\r" +
+        "   margin-top: 10%;\r" +
+        "}\r" +
+        "h3, h4, h5, h6 {\r" +
+        "   text-align: center;\r" +
+        "   margin-bottom: 15%;\r" +
+        "   margin-top: 10%;\r" +
+        "}\r" +
 
         // Style for lists. Calibre sometimes has issues with the placement of lists, this fixes it.
-        "ol, ul {\r"+
-        "   padding-left: 8%;\r"+
-        "}\r"+
+        "ol, ul {\r" +
+        "   padding-left: 8%;\r" +
+        "}\r" +
 
-        "body {\r"+
-        "   margin: 2%;\r"+
-        "}\r"+
+        "body {\r" +
+        "   margin: 2%;\r" +
+        "}\r" +
 
         //Breaks extremely long words, screams, wails etc to fit viewer window.
-        "p {\r"+
-        "   overflow-wrap: break-word;\r"+
-        "   text-indent: 1.5em;\r"+
-        "}\r"+
+        "p {\r" +
+        "   overflow-wrap: break-word;\r" +
+        "   text-indent: 1.5em;\r" +
+        "}\r" +
 
         //Add text intent for <p> tag
-        "p + p {\r"+
-        "   text-indent: 1.5em;\r"+
-        "}\r"+
+        "p + p {\r" +
+        "   text-indent: 1.5em;\r" +
+        "}\r" +
 
         //First letter of first start paragraph with <p> tag.
-        "p:first-letter {\r"+
-        "   text-transform: uppercase;\r"+
-        "}\r"+
+        "p:first-letter {\r" +
+        "   text-transform: uppercase;\r" +
+        "}\r" +
 
-        "h1 + p:first-letter, h3 + p:first-letter, h2 + p:first-letter {\r"+
-        "   font-size: 2em;\r"+
-        "   font-family: cursive;\r"+
-        "   font-weight: bold;\r"+
-        "   font-style: italic;\r"+
-        "   text-shadow: 0 1px 2px rgba(255, 223, 97, 1), 1px 2px 3px black;\r"+
-        "   text-transform: uppercase;\r"+
-        "}\r"+
+        "h1 + p:first-letter, h3 + p:first-letter, h2 + p:first-letter {\r" +
+        "   font-size: 2em;\r" +
+        "   font-family: cursive;\r" +
+        "   font-weight: bold;\r" +
+        "   font-style: italic;\r" +
+        "   text-shadow: 0 1px 2px rgba(255, 223, 97, 1), 1px 2px 3px black;\r" +
+        "   text-transform: uppercase;\r" +
+        "}\r" +
 
-        "h1 + p, h3 + p, h2 + p {\r"+
-        "   text-indent: 1.5em;\r"+
-        "}\r"+
+        "h1 + p, h3 + p, h2 + p {\r" +
+        "   text-indent: 1.5em;\r" +
+        "}\r" +
 
         // Prevent texts inside mutliple definition list tags going outside viewer window.
         // Example https://www.baka-tsuki.org/project/index.php?title=The_Unexplored_Summon_Blood_Sign:Volume2_Opening2
         // It looks okay in a browser but in devices with small screen, it's almost unreadable.
-        "dd, dt, dl {\r"+
-        "   padding: 0;\r"+
-        "   margin: 0;\r"+
-        "}\r"+
+        "dd, dt, dl {\r" +
+        "   padding: 0;\r" +
+        "   margin: 0;\r" +
+        "}\r" +
 
-        "img {\r"+
-        "   display: block;\r"+
-        "   min-height: 1em;\r"+
-        "   max-height: 100%;\r"+
-        "   max-width: 100%;\r"+
-        "   padding-bottom: 0;\r"+
-        "   padding-left: 0;\r"+
-        "   padding-right: 0;\r"+
-        "   padding-top: 0;\r"+
-        "   margin-left: auto;\r"+
-        "   margin-right: auto;\r"+
-        "   margin-bottom: 2%;\r"+
-        "   margin-top: 2%;\r"+
-        "}\r"+
+        "img {\r" +
+        "   display: block;\r" +
+        "   min-height: 1em;\r" +
+        "   max-height: 100%;\r" +
+        "   max-width: 100%;\r" +
+        "   padding-bottom: 0;\r" +
+        "   padding-left: 0;\r" +
+        "   padding-right: 0;\r" +
+        "   padding-top: 0;\r" +
+        "   margin-left: auto;\r" +
+        "   margin-right: auto;\r" +
+        "   margin-bottom: 2%;\r" +
+        "   margin-top: 2%;\r" +
+        "}\r" +
 
         // images embedded in sentances (e.g. Emoji)
-        "img.inline {\r"+
-        "   display: inline;\r"+
-        "   min-height: 1em;\r"+
-        "   margin-bottom: 0;\r"+
-        "   margin-top: 0;\r"+
-        "}\r"+
+        "img.inline {\r" +
+        "   display: inline;\r" +
+        "   min-height: 1em;\r" +
+        "   margin-bottom: 0;\r" +
+        "   margin-top: 0;\r" +
+        "}\r" +
 
-        // differentiate caption text from body text 
-       ".thumbcaption {\r"+
-       "   display: block;\r"+
-       "   font-size: 0.9em;\r"+
-       "   padding-right: 5%;\r"+
-       "   padding-left: 5%;\r"+
-       "}\r"+
-       
+        // differentiate caption text from body text
+        ".thumbcaption {\r" +
+        "   display: block;\r" +
+        "   font-size: 0.9em;\r" +
+        "   padding-right: 5%;\r" +
+        "   padding-left: 5%;\r" +
+        "}\r" +
+
         // To make hr tags more visible. BT doesn't use them very often but other sites might.
-        "hr {\r"+
-        "   color: black;\r"+
-        "   background-color: black;\r"+
-        "   height: 2px;\r"+
-        "}\r"+
+        "hr {\r" +
+        "   color: black;\r" +
+        "   background-color: black;\r" +
+        "   height: 2px;\r" +
+        "}\r" +
 
         // Styling all links.
-        "a:link {\r"+
-        "   text-decoration: none;\r"+
-        "   color: #0B0080;\r"+
-        "}\r"+
-        "a:visited {\r"+
-        "   text-decoration: none;\r"+
-        "}\r"+
-        "a:hover {\r"+
-        "   text-decoration: underline;\r"+
-        "}\r"+
-        "a:active {\r"+
-        "   text-decoration: underline;\r"+
-        "}\r"+
+        "a:link {\r" +
+        "   text-decoration: none;\r" +
+        "   color: #0B0080;\r" +
+        "}\r" +
+        "a:visited {\r" +
+        "   text-decoration: none;\r" +
+        "}\r" +
+        "a:hover {\r" +
+        "   text-decoration: underline;\r" +
+        "}\r" +
+        "a:active {\r" +
+        "   text-decoration: underline;\r" +
+        "}\r" +
 
         // red link for BT non-existent page //
-        "a.new {\r"+
-        "   color: #ba0000;\r"+
-        "}\r"+
+        "a.new {\r" +
+        "   color: #ba0000;\r" +
+        "}\r" +
 
         // some groups use <pre> tag for character status with no wrapping, thus breaking the view in ebook //
-        "pre {\r"+
-        "   white-space: pre-wrap;\r"+
-        "}\r"+
+        "pre {\r" +
+        "   white-space: pre-wrap;\r" +
+        "}\r" +
 
-        "table {\r"+
-        "   width: 90%;\r"+
-        "   border-collapse: collapse;\r"+
-        "}\r"+
-        "table, th, td {\r"+
-        "   border: 1px solid black;\r"+
-        "}\r"+
+        "table {\r" +
+        "   width: 90%;\r" +
+        "   border-collapse: collapse;\r" +
+        "}\r" +
+        "table, th, td {\r" +
+        "   border: 1px solid black;\r" +
+        "}\r" +
 
         // Box around author notes
-        ".webToEpub-author-note {\r"+
-        "   border: 1px solid black;\r"+
-        "   padding: 0.5em;\r"+
-        "}";
+        ".webToEpub-author-note {\r" +
+        "   border: 1px solid black;\r" +
+        "   padding: 0.5em;\r" +
+        "}\r";
     }
 
     static getEpubMetaAddInfo(dom, url, allTags) {
@@ -204,7 +204,7 @@ class EpubMetaInfo {
         }
         return metaAddInfo;
     }
-    
+
     static addSubjectNovelupdate(dom, allTags) {
         let selector = "#seriesgenre .genre";
         if (allTags) {
@@ -216,7 +216,7 @@ class EpubMetaInfo {
     static addDescriptionNovelupdate(dom) {
         return dom.querySelector("#editdescription").textContent.replace(/\n+/g, "\n").replace(/\n/g, "\n\n");
     }
-    
+
     static addAuthorNovelupdate(dom) {
         return dom.querySelector("#authtag").textContent;
     }
