56ca08bff39e25f214baea571df73331b2de1c5d
dteviot <dteviot@gmail.com>
2026-07-01 00:18:20 +1200

    Add site https://novelarrow.com/
    See: https://github.com/dteviot/WebToEpub/issues/2698
    Note, currently still something of a work in progress.

---
+++ plugin/js/parsers/NovelarrowParser.js
@@ -0,0 +1,85 @@
+"use strict";
+
+parserFactory.register("novelarrow.com", () => new NovelarrowParser());
+
+class NovelarrowParser extends Parser { // eslint-disable-line no-unused-vars
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        return [...dom.querySelectorAll(".jsx-6d23f35167deb67d.space-y-6 a")]
+            .map(a => ({
+                sourceUrl: a.href,
+                title: a.querySelector("span.hidden")?.textContent,
+            }))
+            .reverse();
+    }
+
+    findContent(dom) {
+        return Parser.findConstrutedContent(dom);
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("h1");
+    }
+
+    extractAuthor(dom) {
+        let author = dom.querySelector("a[href*='/author/']");
+        return (author === null) ? super.extractAuthor(dom) : author.innerText;    }
+
+    // Optional, supply if individual chapter titles are not inside the content element
+    /*
+    findChapterTitle(dom) {
+        // typical implementation is find node with the Title
+        // Return Title element, OR the title as a string
+        return dom.querySelector("h3.dashhead-title");
+    }
+    */
+
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, ".novel-cover-frame");
+    }
+
+    preprocessRawDom(webPageDom) {
+        let content = this.locateContent(webPageDom);
+        let doc = util.sanitize(`<div class='${Parser.WEB_TO_EPUB_CLASS_NAME}'>"${content}</div>`);
+        let node = doc.querySelector("."+Parser.WEB_TO_EPUB_CLASS_NAME);
+        webPageDom.body.appendChild(node);
+    }
+
+    locateContent(webPageDom) {
+        let encoded = [...webPageDom.querySelectorAll("script")]
+            .filter(s => this.isContent(s))
+            .map(s => this.extractContentString(s.textContent))[0];
+        return encoded
+            ? this.cleanUnicode(encoded)
+            : "";
+    }
+
+    cleanUnicode(s) {
+        return s.replace(/\\u003c/g, "<")
+            .replace(/\\u003e/g, ">")
+            .replace(/\\"/g, "\"")
+            .replace(/\\n/g, "");
+    }
+
+    extractContentString(raw) {
+        let start = raw.indexOf("\"");
+        let end = raw.lastIndexOf("\"");
+        return raw.substring(start + 1, end);
+    }
+
+    isContent(script) {
+        let text = script.textContent;
+        if (text.includes("self.__next_f.push([1,")) {
+            let s = this.extractContentString(text);
+            return s.startsWith("\\u003c");
+        }
+        return false;
+    }
+
+    getInformationEpubItemChildNodes(dom) {
+        return [...dom.querySelectorAll(".site-reading-copy")];
+    }
+}
