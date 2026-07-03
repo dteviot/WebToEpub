0e5a48494f8564e59ab8967929fa806e3beae9cc
Aryan Mane <146648718+ARYAN-9099@users.noreply.github.com>
2026-07-04 03:39:37 +0530

    Add DreamyTranslationsParser (#2767)
    * Add DreamyTranslationsParser
    
    * fixed double quotes bug
    
    * fixed double quotes bug 2
    
    * removed wrong site
    
    * remove DreamyTranslationsParser registerManualSelect
    
    ---------
    
    Co-authored-by: gamebeaker <play4freenew@gmx.de>

---
+++ plugin/js/parsers/DreamyTranslationsParser.js
@@ -0,0 +1,54 @@
+"use strict";
+
+parserFactory.register("dreamy-translations.com", () => new DreamyTranslationsParser());
+
+class DreamyTranslationsParser extends Parser {
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom) {
+        let links = [...dom.querySelectorAll("a")]
+            .filter(a => a.href && a.href.includes("/chapter/") && a.hasAttribute("data-chapter-index"));
+            
+        let chapters = links.map(a => {
+            let title = "";
+            let chNumSpan = a.querySelector("span.font-medium");
+            let titleP = a.querySelector("p.truncate");
+            
+            if (chNumSpan && titleP) {
+                title = chNumSpan.textContent.trim() + " - " + titleP.textContent.trim();
+            } else {
+                title = a.textContent.trim();
+            }
+            
+            return {
+                sourceUrl: a.href,
+                title: title
+            };
+        });
+        
+        return chapters;
+    }
+
+    findCoverImageUrl(dom) {
+        let img = dom.querySelector("img[src*='/covers/']");
+        if (img) {
+            return img.src;
+        }
+        return super.findCoverImageUrl(dom);
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeElements(element.querySelectorAll("sup.tl-note"));
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    findContent(dom) {
+        return dom.querySelector(".chapter-content");
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("button.text-2xl span > span");
+    }
+}
