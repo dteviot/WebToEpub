2db6926ee2b744e6096c473bea6bb3531039a698
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-07-20 06:30:06 +0200

    Add SyosetuOrgParser

---
+++ plugin/js/parsers/SyosetuOrgParser.js
@@ -0,0 +1,54 @@
+"use strict";
+
+parserFactory.register("syosetu.org", () => new SyosetuOrgParser());
+
+class SyosetuOrgParser extends Parser {
+    constructor() {
+        super();
+    }
+
+    async getChapterUrls(dom,) {
+        let menu = dom.querySelector("div.ss table");
+        return util.hyperlinksToChapterList(menu);
+    }
+
+    findContent(dom) {
+        return dom.querySelector("div.ss");
+    }
+
+    extractTitleImpl(dom) {
+        return dom.querySelector("div.ss [itemprop='name']");
+    }
+
+    extractAuthor(dom) {
+        let authorLabel = dom.querySelector("div.ss div [itemprop='author']");
+        return authorLabel?.textContent ?? super.extractAuthor(dom);
+    }
+
+    extractSubject(dom) {
+        let genres = [...dom.querySelectorAll("div.ss [itemprop='genre'] a")];
+        let unavailableTags = [...dom.querySelectorAll("div.ss a.alert_color")];
+        let tags = [...dom.querySelectorAll("div [itemprop='keywords'] a")];
+        return [...genres, ...unavailableTags, ...tags].map(e => e.textContent.trim()).join(", ");
+    }
+
+    extractDescription(dom) {
+        return dom.querySelector("div.ss:nth-child(2)").textContent.trim();
+    }
+
+    removeUnwantedElementsFromContentElement(element) {
+        util.removeChildElementsMatchingSelector(element, "div.ss:nth-child(1) p:nth-child(1), div.novelnavi, #maegaki_open, #atogaki_open");
+        if (this.userPreferences.removeAuthorNotes.value) {
+            util.removeChildElementsMatchingSelector(element, "#maegaki, #atogaki");
+        }
+        super.removeUnwantedElementsFromContentElement(element);
+    }
+
+    populateUIImpl() {
+        document.getElementById("removeAuthorNotesRow").hidden = false;
+    }
+
+    findChapterTitle(dom) {
+        return dom.querySelector("div.ss span");
+    }
+}
