f09f1d0a7672caaa311ed126f45192e3c214588f
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2026-08-11 13:30:33 +0800

    Update AthenaTlsParser

--- plugin/js/parsers/AthenaTlsParser.js
+++ plugin/js/parsers/AthenaTlsParser.js
@@ -10,38 +10,48 @@ class AthenaTlsParser extends Parser {
     }
 
     async getChapterUrls(dom) {
-        return [...dom.querySelectorAll(".volume-container a")]
+        return [...dom.querySelectorAll("a.nv-mokuji__row")]
+            .map(a => {
+                // Remove other unnecessary `span` elements in `a` element.
+                a.innerText = a.querySelector("span.nv-mokuji__t").innerText;
+                return a;
+            })
             .map(a => util.hyperLinkToChapter(a));
     }
 
     findContent(dom) {
-        return dom.querySelector("div.chapter-text-content");
+        return dom.querySelector("article#chapter-content");
     }
 
     extractTitleImpl(dom) {
-        return dom.querySelector("h1.novel-main-title");
+        return dom.querySelector("h1.nv-title");
     }
 
     extractAuthor(dom) {
-        let authorLabel = dom.querySelector("span.creator-name");
+        let authorLabel = dom.querySelector("div.nv-byline span b");
 
         return authorLabel?.textContent ?? super.extractAuthor(dom);
     }
 
     extractSubject(dom) {
-        let tags = [...dom.querySelectorAll(".genre-tag")];
-        return tags.map(e => e.textContent.trim()).join(", ");
+        let tags = [...dom.querySelectorAll(".nv-genres a")];
+        return tags.map(e => {
+            // Remove kanji (?) on the first Genre in Genre List
+            if (e.className === "lead") {
+                // Use Regex pattern in case having more than one kanji in Japanese brackets.
+                e.textContent = e.textContent.replace(/【[^】]*】/,"");
+            }
+            return e.textContent.trim();
+        }).join(", ");
     }
     
     extractDescription(dom) {
-        return dom.querySelector("div.synopsis-content").textContent.trim();
+        return dom.querySelector("section.nv-syn").textContent.trim();
     }
    
     findCoverImageUrl(dom) {
-        let div = dom.querySelector("div.novel-cover-large");
-        
-        let re = /(^url)|[("|")]/g;
-        let imgUrl = div?.style.backgroundImage.replace(re, "") || null;
+        let div = dom.querySelector("div.nv-front__img img");
+        let imgUrl = div?.src || null;
 
         return imgUrl;
     }
