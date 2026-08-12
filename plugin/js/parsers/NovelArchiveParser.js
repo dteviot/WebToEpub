82feb28556642c754f621b0d75034d5e5d655a0d
kerimmkirac <kerimmkirac@gmail.com>
2026-08-12 11:58:41 +0300

    nah

--- plugin/js/parsers/NovelArchiveParser.js
+++ plugin/js/parsers/NovelArchiveParser.js
@@ -19,7 +19,8 @@ class NovelArchiveParser extends Parser {
     }
 
     findCoverImageUrl(dom) {
-        return dom.querySelector("#novel-cover")?.src;
+        return util.getFirstImgSrc(dom, "#novel-cover")
+            ?? dom.querySelector("#novel-cover")?.src;
     }
 
     extractNovelId(dom) {
@@ -38,10 +39,12 @@ class NovelArchiveParser extends Parser {
     async getChapterUrls(dom) {
         let novelId = this.extractNovelId(dom);
         let data = await this.fetchApiJson(`/novels/${novelId}`);
+
         let names = data.novel.chapter_names;
         if (!Array.isArray(names)) {
             throw new Error("Unexpected novel response for id " + novelId);
         }
+
         return names.map((name, index) => ({
             sourceUrl: `${this.apiBase}/novels/${novelId}/chapters/${index + 1}`,
             title: name
@@ -56,7 +59,11 @@ class NovelArchiveParser extends Parser {
         }
 
         let newDoc = Parser.makeEmptyDocForContent(url);
-        Parser.addTextToChapterContent(newDoc.content, chapterData.content);
+        chapterData.content.split(/\n+/).filter((line) => line.trim() !== "").forEach((line) => {
+            let p = newDoc.dom.createElement("p");
+            p.textContent = line.trim();
+            newDoc.content.appendChild(p);
+        });
         return newDoc.dom;
     }
 
